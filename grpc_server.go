456e8d7783e704567e7268d86d8fe110b8948442
KHUSHAL_SHAH <khushalshah021098@gmail.com>
2019-04-16 17:48:14 +0530

    Added Realtime module for GRPC

--- grpc_server.go
+++ grpc_server.go
@@ -679,7 +679,7 @@ func (s *server) Call(ctx context.Context, in *pb.FaaSRequest) (*pb.Response, er
 		return &out, nil
 	}
 
-	resultBytes,err := s.faas.Operation(s.auth,in.Token,in.Engine,in.Function,int(in.Timeout))
+	resultBytes, err := s.faas.Operation(s.auth, in.Token, in.Engine, in.Function, int(in.Timeout))
 	if err != nil {
 		out := pb.Response{}
 		out.Status = 500
@@ -690,4 +690,103 @@ func (s *server) Call(ctx context.Context, in *pb.FaaSRequest) (*pb.Response, er
 	out.Result = resultBytes
 	out.Status = 200
 	return &out, nil
+}
+
+func (s *server) RealTime(stream pb.SpaceCloud_RealTimeServer) error {
+	client := utils.CreateGRPCClient(stream)
+	defer client.Close()
+	defer s.realtime.RemoveClient(client.ClientID())
+	client.RoutineWrite()
+
+	client.Read(func(req *model.Message) {
+
+		switch req.Type {
+		case utils.TypeRealtimeSubscribe:
+			// For realtime subscribe event
+			data := new(model.RealtimeRequest)
+			mapstructure.Decode(req.Data, data)
+
+			// Check if the user is authenticated
+			authObj, err := auth.IsAuthenticated(data.Token, data.DBType, data.Group, utils.Read)
+			if err != nil {
+				client.Write(model.Message{
+					ID:   req.ID,
+					Type: req.Type,
+					Data: model.RealtimeResponse{Group: data.Group, ID: data.ID, Ack: false, Error: err.Error()},
+				})
+				return
+			}
+
+			// Create an args object
+			args := map[string]interface{}{
+				"args":    map[string]interface{}{"find": data.Where, "op": utils.All, "auth": authObj},
+				"project": data.Project, // Don't forget to do this for every request
+			}
+
+			// Check if user is authorized to make this request
+			err = auth.IsAuthorized(data.DBType, data.Group, utils.Read, args)
+			if err != nil {
+				client.Write(model.Message{
+					ID:   req.ID,
+					Type: req.Type,
+					Data: model.RealtimeResponse{Group: data.Group, ID: data.ID, Ack: false, Error: err.Error()},
+				})
+				return
+			}
+
+			readReq := model.ReadRequest{Find: data.Where, Operation: utils.All}
+			result, err := crud.Read(client.Context, data.DBType, data.Project, data.Group, &readReq)
+			if err != nil {
+				client.Write(model.Message{
+					ID:   req.ID,
+					Type: req.Type,
+					Data: model.RealtimeResponse{Group: data.Group, ID: data.ID, Ack: false, Error: err.Error()},
+				})
+				return
+			}
+
+			feedData := []*model.FeedData{}
+			array, ok := result.([]interface{})
+			if ok {
+				timeStamp := time.Now().Unix()
+				for _, row := range array {
+					payload := row.(map[string]interface{})
+					idVar := "id"
+					if data.DBType == string(utils.Mongo) {
+						idVar = "_id"
+					}
+					if docID, ok := payload[idVar].(string); ok {
+						feedData = append(feedData, &model.FeedData{
+							Group:     data.Group,
+							Type:      utils.RealtimeWrite,
+							TimeStamp: timeStamp,
+							DocID:     docID,
+							DBType:    data.DBType,
+							Payload:   payload,
+							QueryID:   data.ID,
+						})
+					}
+				}
+			}
+			// Add the live query
+			realtime.AddLiveQuery(data.ID, data.Group, client, data.Where)
+			client.Write(model.Message{
+				ID:   req.ID,
+				Type: req.Type,
+				Data: model.RealtimeResponse{Group: data.Group, ID: data.ID, Ack: true, Docs: feedData},
+			})
+
+		case utils.TypeRealtimeUnsubscribe:
+			// For realtime unsubscribe event
+			data := new(model.RealtimeRequest)
+			mapstructure.Decode(req.Data, data)
+
+			realtime.RemoveLiveQuery(data.Group, client.ClientID(), data.ID)
+			client.Write(model.Message{
+				ID:   req.ID,
+				Type: req.Type,
+				Data: model.RealtimeResponse{Group: data.Group, ID: data.ID, Ack: true},
+			})
+		}
+	})
 }
--- utils/client.go
+++ utils/client.go
@@ -2,21 +2,25 @@ package utils
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	"github.com/gorilla/websocket"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/spaceuptech/space-cloud/model"
+	pb "github.com/spaceuptech/space-cloud/proto"
 )
 
 // Client is the object handling all client interactions
 type Client struct {
-	id      string
-	channel chan interface{}
-	Context context.Context
-	cancel  context.CancelFunc
-	socket  *websocket.Conn
+	id       string
+	channel  chan *model.Message
+	Context  context.Context
+	cancel   context.CancelFunc
+	socket   *websocket.Conn              //only for Websocket
+	stream   pb.SpaceCloud_RealTimeServer //Only for grpc
+	protocol string
 }
 
 // DataCallback is the callback invoked when data is read by the socket
@@ -24,18 +28,56 @@ type DataCallback func(data *model.Message)
 
 // RoutineWrite starts a json writer routine
 func (c *Client) RoutineWrite() {
-	go func() {
-		for res := range c.channel {
-			err := c.socket.WriteJSON(res)
-			if err != nil {
-				log.Println(err)
+	if c.protocol == "WEBSOCKET" {
+		go func() {
+			for res := range c.channel {
+				err := c.socket.WriteJSON(res)
+				if err != nil {
+					log.Println(err)
+				}
 			}
-		}
-	}()
+		}()
+	} else if c.protocol == "GRPC" {
+		go func() {
+			for res := range c.channel {
+				//Convert the Message into RealTime response.
+				switch res.Type {
+				case TypeRealtimeSubscribe, TypeRealtimeUnsubscribe:
+					//Decode the Message
+					var feedData []*pb.FeedData
+					responseMsg := res.Data.(model.RealtimeResponse)
+					for i, feed := range responseMsg.Docs {
+						payload, err := json.Marshal(feed.Payload)
+						if err != nil {
+							grpcResponse := pb.RealTimeResponse{Error: err.Error()}
+							c.stream.Send(&grpcResponse)
+						}
+						feedData[i] = &pb.FeedData{QueryId: feed.QueryID, DocId: feed.DocID, Type: feed.Type, Group: feed.Group, DbType: feed.DBType, Payload: payload, TimeStamp: feed.TimeStamp}
+					}
+					grpcResponse := pb.RealTimeResponse{Id: res.ID, Group: responseMsg.Group, Ack: responseMsg.Ack, Error: responseMsg.Error, FeedData: feedData}
+					c.stream.Send(&grpcResponse)
+
+				case TypeRealtimeFeed:
+					feed := res.Data.(model.FeedData)
+					var feedData []*pb.FeedData
+					payload, err := json.Marshal(feed.Payload)
+					if err != nil {
+						grpcResponse := pb.RealTimeResponse{Error: err.Error()}
+						c.stream.Send(&grpcResponse)
+					}
+					feedData[0] = &pb.FeedData{
+						QueryId: feed.QueryID, DocId: feed.DocID, Type: feed.Type, Group: feed.Group, DbType: feed.DBType, Payload: payload, TimeStamp: feed.TimeStamp}
+					grpcResponse := pb.RealTimeResponse{Id: res.ID, Group: res.Data.(model.FeedData).Group, FeedData: feedData}
+					c.stream.Send(&grpcResponse)
+				}
+			}
+		}()
+	}
+
 }
 
 // Write wrties the object to the client
-func (c *Client) Write(res interface{}) {
+func (c *Client) Write(res *model.Message) {
 	select {
 	case c.channel <- res:
 	case <-c.Context.Done():
@@ -51,16 +93,38 @@ func (c *Client) Close() {
 
 // Read startes a blocking reader routine
 func (c *Client) Read(cb DataCallback) {
-	defer c.Close()
-	for {
-		data := &model.Message{}
-		err := c.socket.ReadJSON(data)
-		if err != nil {
-			return
-		}
+	if c.protocol == "WEBSOCKET" {
+		defer c.Close()
+		for {
+			data := &model.Message{}
+			err := c.socket.ReadJSON(data)
+			if err != nil {
+				return
+			}
 
-		cb(data)
+			cb(data)
+		}
+	} else if c.protocol == "GRPC" {
+		defer c.Close()
+		for {
+			in, err := c.stream.Recv()
+			if err != nil {
+				return
+			}
+			var data map[string]interface{}
+			data["Token"] = in.Token
+			data["DBType"] = in.DbType
+			data["Project"] = in.Project
+			data["Group"] = in.Group
+			data["Type"] = in.Type
+			data["ID"] = in.Id
+			data["Where"] = in.Where
+
+			msg := &model.Message{Type: in.Type, ID: in.Id, Data: data}
+			cb(msg)
+		}
 	}
+
 }
 
 // ClientID returns the client's id
@@ -70,8 +134,16 @@ func (c *Client) ClientID() string {
 
 // CreateWebsocketClient makes a client object to manage the socket
 func CreateWebsocketClient(socket *websocket.Conn) *Client {
-	channel := make(chan interface{}, 5)
+	channel := make(chan *model.Message, 5)
 	ctx, cancel := context.WithCancel(context.Background())
 	id := uuid.NewV1().String()
-	return &Client{id, channel, ctx, cancel, socket}
+	return &Client{id, channel, ctx, cancel, socket, nil, "WEBSOCKET"}
+}
+
+// CreateGRPCClient makes a client object to manage the grpc
+func CreateGRPCClient(stream pb.SpaceCloud_RealTimeServer) *Client {
+	channel := make(chan *model.Message, 5)
+	ctx, cancel := context.WithCancel(context.Background())
+	id := uuid.NewV1().String()
+	return &Client{id, channel, ctx, cancel, nil, stream, "GRPC"}
 }
