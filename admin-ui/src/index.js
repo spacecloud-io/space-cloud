7a7cec6e38e1f496b62326ffa5505168efe2cbd2
ShreyaLaheri <slaheri99@gmail.com>
2019-06-05 16:50:01 +0530

    finished login page

--- admin-ui/src/index.js
+++ admin-ui/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css'
+
 import { Provider } from "react-redux";
 import store from "./store";
 
--- admin-ui/src/pages/login/Login.jsx
+++ admin-ui/src/pages/login/Login.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import background from '../../assets/Background.svg';
+import Header from '../../components/header/Header'
+import './login.css';
+import LoginForm from './LoginForm';
+import { set, get } from "automate-redux";
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>Login</div>
-    )
-  }
+function Login(props) {
+  return (
+    <div className="wrapper" >
+      <div className="image">
+        <img className="background" src={background} />
+        <div className="rText">
+          <Header name="Welcome to Space Cloud !" color="#FFF" fontSize="28px" />
+          <p className="text">Develop great applications without having to write backend code.
+              Focus more on business and less on technology.</p>
+        </div>
+      </div>
+      <LoginForm formState={props.formState} isLoading={props.isLoading} updateFormState={props.updateFormState} handleSubmit={props.handleSubmit} />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
   return {
-
+    formState: state.uiState.login.formState,
+    isLoading: state.uiState.login.isLoading,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    updateFormState: (fields) => {
+      console.log(fields)
+      dispatch(set("uiState.login.formState", fields))
+    },
+    handleSubmit: () => {
+      console.log('Submitted')
+    }
   }
 }
 
