a02f60209c1a75486083636ba1fffbb8fd894da0
SarveshKargutkar <sckargutkar@gmail.com>
2019-06-14 15:49:10 +0530

    rules changes done

--- admin-ui/src/components/rules/rules.jsx
+++ admin-ui/src/components/rules/rules.jsx
@@ -1,44 +1,82 @@
-import React, { Component } from 'react'
-// import './rules.css'
-import { connect } from 'react-redux'
-import { Row ,Col} from 'antd';
+import React, { Component } from 'react';
+import './rules.css';
+import { Row, Col, Icon } from 'antd';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+import 'codemirror/theme/material.css';
+import 'codemirror/lib/codemirror.css';
 
 class Rules extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    };
+	constructor(props) {
+		super(props);
+		const rules = Object.keys(this.props.rules);
+		var selectedRule = '';
+		if (rules.length > 0) {
+			selectedRule = rules[0];
+		}
+		this.state = { selectedRule: selectedRule };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleChange(editor, data, value) {
+		console.log(value);
+		this.props.handleRuleChange(value);
+	}
+	handleClick(rule) {
+		this.setState({ selectedRule: rule });
+	}
+	render() {
+		var rules = Object.keys(this.props.rules);
+		const values = Object.values(this.props.rules);
+		const index = rules.indexOf(this.state.selectedRule);
 
-  }
-
-
-  render() {
-    return (
-      <div className="rules-main-wrapper">
-      <Row>
-        <Col span={6} >
-       {this.props.rules.keys.map((rule)=>{
-         <p>{rule}</p>
-       })}
-       </Col>
-        <Col span={18} >
-        vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        </Col>
-        </Row>
-      </div>
-    )
-  }
+		return (
+			<div className="rules-main-wrapper">
+				<Row>
+					<Col span={6}>
+						<div className="addaRule" onClick={this.props.handleAddTextClick}>
+							<Icon className="addIcon" type="plus" /> {this.props.addText}
+						</div>
+						<div className="rulesTable">
+							{rules.map((rule) => {
+								return (
+									<div
+										className={`rule ${this.state.selectedRule === rule ? 'selected' : ''}`}
+										id="rule"
+										value={rule}
+										key={rule}
+										onClick={() => this.handleClick(rule)}
+									>
+										{rule}
+									</div>
+								);
+							})}
+						</div>
+					</Col>
+					<Col span={18}>
+						<div className="code">
+							<div className="code-hint">
+								Hint : To indent press ctrl + A in the editor and then shift + tab
+							</div>
+							<div className="code-mirror">
+								<CodeMirror
+									value={values[index]}
+									options={{
+										mode: 'xml',
+										// theme: 'material',
+										autofocus: true,
+										lineNumbers: true
+									}}
+									onBeforeChange={(editor, data, value) => {
+										this.handleChange(editor, data, value);
+									}}
+								/>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-   
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Rules);
+export default Rules;
--- admin-ui/src/pages/database/rules/Rules.jsx
+++ admin-ui/src/pages/database/rules/Rules.jsx
@@ -1,59 +1,74 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import '../../../index.css'
-import Sidenav from '../../../components/sidenav/Sidenav'
-import Topbar from '../../../components/topbar/Topbar'
-import Header from '../../../components/header/Header'
-import Documentation from '../../../components/documentation/Documentation'
-import DbConfigure from '../../../components/database-rules/DbConfigure'
-import EmptyState from '../../../components/rules/EmptyState'
-import rulesImg from '../../../assets/rules.svg'
-import RulesTable  from '../../../components/rules/rules'
+import React from 'react';
+import { connect } from 'react-redux';
+import '../../../index.css';
+import Sidenav from '../../../components/sidenav/Sidenav';
+import Topbar from '../../../components/topbar/Topbar';
+import Header from '../../../components/header/Header';
+import Documentation from '../../../components/documentation/Documentation';
+import DbConfigure from '../../../components/database-rules/DbConfigure';
+import EmptyState from '../../../components/rules/EmptyState';
+import rulesImg from '../../../assets/rules.svg';
+import RulesTable from '../../../components/rules/rules';
 
 function Rules(props) {
-  return (
-    <div>
-      <Topbar title="Database" />
-      <div className="flex-box">
-        <Sidenav selectedItem="database" />
-        <div className="page-content">
-          <div className="header-flex">
-            <Header name="Rules" color="#000" fontSize="22px" />
-            <Documentation url="https://spaceuptech.com/docs/database" />
-          </div>
-          <DbConfigure updateFormState={props.updateFormState} formState={props.formState} />
-          {/* <EmptyState graphics={rulesImg} desc="Guard your data with rules that define who has access to it and how it is structured." buttonText="Add a table" handleClick={props.handleClick} /> */}
-          <RulesTable rules={props.rules}/>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<Topbar title="Database" />
+			<div className="flex-box">
+				<Sidenav selectedItem="database" />
+				<div className="page-content">
+					<div className="header-flex">
+						<Header name="Rules" color="#000" fontSize="22px" />
+						<Documentation url="https://spaceuptech.com/docs/database" />
+					</div>
+					<DbConfigure updateFormState={props.updateFormState} formState={props.formState} />
+					{/* <EmptyState graphics={rulesImg} desc="Guard your data with rules that define who has access to it and how it is structured." buttonText="Add a table" handleClick={props.handleClick} /> */}
+					<RulesTable
+						rules={props.rules}
+						handleRuleChange={props.handleRuleChange}
+						addText={props.addText}
+						handleAddTextClick={props.handleAddTextClick}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    formState: {
-      enabled: true,
-      conn: "http://localhost/8080"
-    },
-    rules:{
-      Books:'',
-      Todos:'',
-      Users:''
-    }
-  }
-}
+	return {
+		formState: {
+			enabled: true,
+			conn: 'http://localhost/8080'
+		},
+		rules: {
+			Books: 'a',
+			Todos: 'b',
+			Users: 'c',
+			xyz: 'd',
+			sas: 'e',
+			qwqw: 'f',
+			rewwr: 'g'
+		},
+		addText: 'Add a table rule'
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    handleClick: () => {
-      console.log("Checked")
+	return {
+		handleClick: () => {
+			console.log('Checked');
+		},
+		handleRuleChange: (value) => {
+			console.log('Rule Changed', value);
     },
-
-    updateFormState: (fields) => {
-      console.log(fields)
+    handleAddTextClick:()=>{
+			console.log('Table Added');
     },
-  }
-}
+		updateFormState: (fields) => {
+			console.log(fields);
+		}
+	};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Rules);
