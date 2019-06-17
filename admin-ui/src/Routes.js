e7f2af77e051020971438eae68a13fcbb8106add
Shreya Laheri <48062358+ShreyaLaheri@users.noreply.github.com>
2019-06-17 13:00:51 +0530

    Overview done. (#272)
    Made Overview page

--- admin-ui/src/Routes.js
+++ admin-ui/src/Routes.js
@@ -4,13 +4,14 @@ import history from './history';
 import Login from './pages/login/Login'
 import Database from './pages/database/Database'
 import Rules from './pages/database/rules/Rules'
-
+import Overview from  './pages/overview/Overview'
 
 export default () => {
   return (
     <Router history={history}>
       <Route exact path="/" component={Login} />
       <Route exact path="/:projectId/database" component={Database} />
+      <Route exact path="/:projectId/overview" component={Overview} />
       <Route exact path="/:projectId/database/rules/:database" component={Rules} />
     </Router>
   )
