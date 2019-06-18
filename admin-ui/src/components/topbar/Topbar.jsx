23bc9c7a6ff4a5bd52f4669cbeb4c59a01b33a51
ShreyaLaheri <slaheri99@gmail.com>
2019-06-18 10:20:13 +0530

    fixed copy to clipboard imports

--- admin-ui/src/components/topbar/Topbar.jsx
+++ admin-ui/src/components/topbar/Topbar.jsx
@@ -11,11 +11,11 @@ function Topbar(props) {
     <div className="topbar">
       <img className="logo-black" src={logo} alt="logo" />
       <span>{props.title}</span>
-      {(props.title === "Database")? 
-      <DbSelector handleSelect={props.handleSelect} selectedDb={props.selectedDb} />:
+      {props.title === "Database" &&
+        <DbSelector handleSelect={props.handleSelect} selectedDb={props.selectedDb} />
+      }
       <Button type="primary" className="save-button" onClick={props.handleSave}>SAVE</Button>
-    }
-      </div>
+    </div>
   )
 }
 
