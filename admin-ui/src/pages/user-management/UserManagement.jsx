fa900738f9b3b5ce127eef59c6113269ac11735c
Jayesh <jayesh@spaceuptech.com>
2019-06-22 18:03:52 +0530

    minor changes

--- admin-ui/src/pages/user-management/UserManagement.jsx
+++ admin-ui/src/pages/user-management/UserManagement.jsx
@@ -48,23 +48,6 @@ function UserManagement(props) {
               </Panel>
             </Collapse><br />
           </div>
-          <Collapse accordion expandIconPosition="right" expandIcon={({ isActive }) => <Icon type="right" rotate={isActive ? 270 : 90} />}>
-            <Panel header={(<CollapseHeader icon={mailIcon} desc="Mail" />)} key="1">
-              <Email formState={props.email} handleChange={(values) => props.handleChange("email", values)}/>
-            </Panel>
-            <Panel header={(<CollapseHeader icon={googleIcon} desc="Google" />)} key="2">
-              <Oauth formState={props.google} type="Google" redirectUrl="www.google.com" handleChange={(values) => props.handleChange("google", values)} />
-            </Panel >
-            <Panel header={(<CollapseHeader icon={fbIcon} desc="Facebook" />)} key="3">
-              <Oauth formState={props.fb} type="Facebook" redirectUrl="www.fb.com" handleChange={(values) => props.handleChange("fb", values)} />
-            </Panel>
-            <Panel header={(<CollapseHeader icon={twitterIcon} desc="Twitter" />)} key="4">
-              <Oauth formState={props.twitter} type="Twitter" redirectUrl="www.twitter.com" handleChange={(values) => props.handleChange("twitter", values)} />
-            </Panel>
-            <Panel header={(<CollapseHeader icon={githubIcon} desc="Github" />)} key="5">
-              <Oauth formState={props.github} type="Github" redirectUrl="www.github.com" handleChange={(values) => props.handleChange("github", values)} />
-            </Panel>
-          </Collapse><br />
         </div>
       </div>
     </div>
