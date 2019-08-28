bf25e3f43b92e9fcc2d4012cf7e6fb94a89af7db
sharadregoti <sharadregoti15@gmail.com>
2019-08-28 12:50:45 +0530

    added schema validation function

--- modules/crud/schema.go
+++ modules/crud/schema.go
@@ -3,17 +3,21 @@ package crud
 import (
 	"errors"
 	"strings"
+	"time"
 
 	"github.com/graphql-go/graphql/language/ast"
 	"github.com/graphql-go/graphql/language/kinds"
 	"github.com/graphql-go/graphql/language/parser"
 	"github.com/graphql-go/graphql/language/source"
 	"github.com/spaceuptech/space-cloud/config"
+	"github.com/spaceuptech/space-cloud/model"
 	"github.com/spaceuptech/space-cloud/utils"
 )
 
+// TODO: check graphql types
+
 // ParseSchema Initializes Schema field in Module struct
-func (m *Module) ParseSchema(crud config.Crud) (SchemaType, error) {
+func (m *Module) ParseSchema(crud config.Crud) error {
 	schema := make(SchemaType, len(crud))
 	for dbName, v := range crud {
 		collection := SchemaCollection{}
@@ -24,17 +28,18 @@ func (m *Module) ParseSchema(crud config.Crud) (SchemaType, error) {
 			// parse the source
 			doc, err := parser.Parse(parser.ParseParams{Source: source})
 			if err != nil {
-				return nil, err
+				return err
 			}
 			value, err := getCollectionSchema(doc, collectionName)
 			if err != nil {
-				return nil, err
+				return err
 			}
 			collection[collectionName] = value
 		}
 		schema[dbName] = collection
 	}
-	return schema, nil
+	m.schema = schema
+	return nil
 }
 
 func getCollectionSchema(doc *ast.Document, collectionName string) (SchemaField, error) {
@@ -132,7 +137,116 @@ func getFieldType(fieldType ast.Type, fieldTypeStuct *SchemaFieldType, doc *ast.
 	return nil
 }
 
+func (m *Module) schemaValidator(dbType, col string, fields map[string]interface{}) error {
+
+	collectionFields := m.schema[dbType][col]
+	for fieldKey, fieldValue := range collectionFields {
+		// TODO: check if internal elements of list is required for this we need to change schmea parser
+		// check if key is required
+		value, ok := fields[fieldKey]
+		if fieldValue.IsFieldTypeRequired {
+			if !ok {
+				return errors.New("Field " + fieldKey + " Not Present")
+			}
+		}
+
+		// check type
+		fieldType, err := m.checkType(value, fieldValue.Kind, dbType, fieldValue.TableJoin)
+		if err != nil {
+			return err
+		}
+
+		if fieldType != fieldValue.Kind {
+			return errors.New("Wrong Type Wanter " + fieldValue.Kind + " got " + fieldType)
+		}
+
+	}
+
+	return nil
+}
+
 // ValidateSchema checks data type
-func (m *Module) ValidateSchema() {
+func (m *Module) ValidateSchema(dbType, col string, req *model.CreateRequest) error {
+
+	v := make([]map[string]interface{}, 1)
+
+	switch t := req.Document.(type) {
+	case []map[string]interface{}:
+		v = t
+	case map[string]interface{}:
+		v = append(v, t)
+	}
+
+	for _, fields := range v {
+		err := m.schemaValidator(dbType, col, fields)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *Module) checkType(value interface{}, kind, dbType, col string) (string, error) {
+
+	switch v := value.(type) {
+	case int:
+		// TODO: int64
+		switch kind {
+		case TypeDateTime:
+			unitTimeInRFC3339 := time.Unix(int64(v), 0).Format(time.RFC3339)
+			if unitTimeInRFC3339 == "" {
+				return "", errors.New("Integer Wrong Date-Time Format")
+			}
+			return TypeDateTime, nil
+		case TypeID:
+			return TypeID, nil
+		}
+		return TypeInteger, nil
+
+	case string:
+		switch kind {
+		case TypeDateTime:
+			_, err := time.Parse(time.RFC3339, v)
+			if err != nil {
+				return "", errors.New("String Wrong Date-Time Format")
+			}
+			return TypeDateTime, nil
+		case TypeID:
+			return TypeID, nil
+		}
+		return TypeString, nil
+
+	case float32, float64:
+		return TypeFloat, nil
+
+	case bool:
+		return TypeBoolean, nil
+
+	case map[string]interface{}:
+		err := m.schemaValidator(dbType, col, v)
+		if err != nil {
+			return "", err
+		}
+		return TypeJoin, nil
+
+	case []interface{}:
+		var str string
+		var err error
+
+		for _, value := range v {
+			str, err = m.checkType(value, kind, dbType, col)
+			if err != nil {
+				return "", err
+			}
+
+			if str != kind {
+				return "", errors.New("Wrong List Type Wanted " + kind + " got " + str)
+			}
+		}
+		return str, nil
+
+	}
+	return "", errors.New("Checktype no match found")
 
 }
--- modules/crud/schema_test.go
+++ modules/crud/schema_test.go
@@ -1,78 +1,119 @@
 package crud
 
 import (
-	"encoding/json"
-	"fmt"
 	"testing"
+
+	"github.com/spaceuptech/space-cloud/model"
 
 	"github.com/spaceuptech/space-cloud/config"
 )
 
+var query = `
+type Tweet {
+	id: ID! @id
+	createdAt: DateTime! @createdAt
+	text: String
+	owner: [Integer!]! @relation(link: INLINE)
+	location: Location!
+  }
+  
+  type User {
+	id: ID! @id
+	createdAt: DateTime! @createdAt
+	updatedAt: DateTime! @updatedAt
+	handle: String! @unique
+	name: String
+	tweets: [Tweet!]!
+  }
+  
+  type Location {
+	id: ID! @id
+	latitude: Float!
+	longitude: Float!
+  }
+`
+var v = config.Crud{
+	"mongo": &config.CrudStub{
+		Collections: map[string]*config.TableRule{
+			"tweet": &config.TableRule{
+				Schema: query,
+			},
+			"location": &config.TableRule{
+				Schema: query,
+			},
+			"user": &config.TableRule{
+				Schema: query,
+			},
+		},
+	},
+}
+
 func TestParseSchema(t *testing.T) {
-	query := `
-	type Tweet {
-		id: ID! @id
-		createdAt: DateTime! @createdAt
-		text: String
-		owner: [User!]! @relation(link: INLINE)
-		location: Location!
-	  }
-	  
-	  type User {
-		id: ID! @id
-		createdAt: DateTime! @createdAt
-		updatedAt: DateTime! @updatedAt
-		handle: String! @unique
-		name: String
-		tweets: [Tweet!]!
-	  }
-	  
-	  type Location {
-		id: ID! @id
-		latitude: Float!
-		longitude: Float!
-	  }
-	`
-	v := config.Crud{
-		"mongo": &config.CrudStub{
-			Collections: map[string]*config.TableRule{
-				"tweet": &config.TableRule{
-					Schema: query,
-				},
-				"location": &config.TableRule{
-					Schema: query,
-				},
-				"user": &config.TableRule{
-					Schema: query,
-				},
-			},
-		},
-		"sql": &config.CrudStub{
-			Collections: map[string]*config.TableRule{
-				"tweet": &config.TableRule{
-					Schema: query,
-				},
-				"location": &config.TableRule{
-					Schema: query,
-				},
-				"user": &config.TableRule{
-					Schema: query,
-				},
-			},
-		},
-	}
+
 	m := Init()
 
 	t.Run("Schema Parser", func(t *testing.T) {
-		output, err := m.ParseSchema(v)
+		err := m.ParseSchema(v)
 		if err != nil {
 			t.Fatal(err)
 		}
-		b, err := json.MarshalIndent(output, "", "  ")
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-		fmt.Print(string(b))
-		t.Log("Logging Test Output :: ", output)
+		// b, err := json.MarshalIndent(m.schema, "", "  ")
+		// if err != nil {
+		// 	fmt.Println("error:", err)
+		// }
+		// fmt.Print(string(b))
+		t.Log("Logging Test Output :: ", m.schema)
 	})
+}
+
+func TestValidateSchema(t *testing.T) {
+
+	var arr []interface{}
+	// str := []string{"sharad", "regoti", "atharva"}
+	str := []int{1, 2, 3}
+	for _, v := range str {
+		arr = append(arr, v)
+	}
+
+	req := model.CreateRequest{
+		Document: []map[string]interface{}{
+			{
+				"id":        "dfdsairfa",
+				"createdAt": 986413662654,
+				"text":      "Hello World!",
+				"location": map[string]interface{}{
+					"id":        "locatoinid",
+					"latitude":  5.6,
+					"longitude": 312.3,
+				},
+				"owner": arr,
+			},
+		},
+	}
+
+	tdd := []struct {
+		dbName, coll, description string
+		value                     model.CreateRequest
+	}{{
+		dbName:      "mongo",
+		coll:        "tweet",
+		description: "checking User defined type",
+		value:       req,
+	}}
+
+	m := Init()
+	err := m.ParseSchema(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, val := range tdd {
+		t.Run(val.description, func(t *testing.T) {
+			err := m.ValidateSchema(val.dbName, val.coll, &val.value)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+
 }
