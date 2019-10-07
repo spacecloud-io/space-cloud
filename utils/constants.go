584e473233ff2ee2e7d96181579428e6f6076c24
YourTechBud <noorain@spaceuptech.com>
2019-10-07 17:15:20 +0530

    added an endpoint to queue a new event

--- utils/constants.go
+++ utils/constants.go
@@ -1,7 +1,7 @@
 package utils
 
 // BuildVersion is the current version of Space Cloud
-const BuildVersion = "0.12.0"
+const BuildVersion = "0.12.1"
 
 const (
 	// One operation returns a single document from the database
