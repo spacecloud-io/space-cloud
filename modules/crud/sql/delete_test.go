16b88fe503332c81c7387299cec0f400c1e87c29
Khannaabhi <abhishekakhanna@gmail.com>
2019-12-28 18:05:45 +0530

    test for create, delete and read

--- modules/crud/sql/delete_test.go
+++ modules/crud/sql/delete_test.go
@@ -1,7 +1,6 @@
 package sql
 
 import (
-	"context"
 	"testing"
 
 	"github.com/spaceuptech/space-cloud/model"
@@ -9,27 +8,51 @@ import (
 
 func TestGenerateDeleteQuery(t *testing.T) {
 	tests := []struct {
-		name, tableName, wantThis, orThis string
-		req                               model.DeleteRequest
+		name, dbType, tableName, wantThis, orThis string
+		req                                       model.DeleteRequest
 	}{
-		{name: "Successfull Test", tableName: "fooTable", orThis: "DELETE FROM fooTable WHERE ((String2 = ?) AND (String1 = ?))", wantThis: "DELETE FROM fooTable WHERE ((String1 = ?) AND (String2 = ?))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": "1", "String2": "2"}}},
-		{name: "Nested Map Interface Equal To", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 = ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$eq": 1}}}},
-		{name: "Nested Map Interface Not Equal To", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 != ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$ne": 1}}}},
-		{name: "Nested Map Interface Greater than ", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 > ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gt": 1}}}},
-		{name: "Nested Map Interface Greater than Equal To", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 >= ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gte": 1}}}},
-		{name: "Nested Map Interface Less Than", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 < ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lt": 1}}}},
-		{name: "Nested Map Interface Less Than Equal To", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 <= ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lte": 1}}}},
-		{name: "Nested Map Interface In", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 IN (?))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$in": 1}}}},
-		{name: "Nested Map Interface Not in", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE (String1 NOT IN (?))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$nin": 1}}}},
-		{name: "Nested Map Interface OR", tableName: "fooTable", wantThis: "DELETE FROM fooTable WHERE ((string1ofstring1 = ?) OR (string1ofstring2 = ?))", req: model.DeleteRequest{Find: map[string]interface{}{"$or": []interface{}{map[string]interface{}{"string1ofstring1": "1"}, map[string]interface{}{"string1ofstring2": "2"}}}}},
-		{name: "When length is 0", tableName: "fooTable", wantThis: "DELETE FROM fooTable", req: model.DeleteRequest{Find: map[string]interface{}{}}},
+		{name: "Successfull Test", dbType: "mysql", tableName: "fooTable", orThis: "DELETE FROM projectName.fooTable WHERE ((String1 = ?) AND (String2 = ?))", wantThis: "DELETE FROM projectName.fooTable WHERE ((String1 = ?) AND (String2 = ?))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": "1", "String2": "2"}}},
+		{name: "Nested Map Interface Equal To", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 = ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$eq": 1}}}},
+		{name: "Nested Map Interface Not Equal To", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 != ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$ne": 1}}}},
+		{name: "Nested Map Interface Greater than ", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 > ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gt": 1}}}},
+		{name: "Nested Map Interface Greater than Equal To", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 >= ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gte": 1}}}},
+		{name: "Nested Map Interface Less Than", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 < ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lt": 1}}}},
+		{name: "Nested Map Interface Less Than Equal To", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 <= ?)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lte": 1}}}},
+		{name: "Nested Map Interface In", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 IN (?))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$in": 1}}}},
+		{name: "Nested Map Interface Not in", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 NOT IN (?))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$nin": 1}}}},
+		{name: "Nested Map Interface OR", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE ((string1ofstring1 = ?) OR (string1ofstring2 = ?))", req: model.DeleteRequest{Find: map[string]interface{}{"$or": []interface{}{map[string]interface{}{"string1ofstring1": "1"}, map[string]interface{}{"string1ofstring2": "2"}}}}},
+		{name: "When length is 0", dbType: "mysql", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable", req: model.DeleteRequest{Find: map[string]interface{}{}}},
+
+		{name: "Successfull Test", dbType: "sqlserver", tableName: "fooTable", orThis: "DELETE FROM projectName.fooTable WHERE ((String2 = @p1) AND (String1 = @p2))", wantThis: "DELETE FROM projectName.fooTable WHERE ((String1 = @p1) AND (String2 = @p2))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": "1", "String2": "2"}}},
+		{name: "Nested Map Interface Equal To", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 = @p1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$eq": 1}}}},
+		{name: "Nested Map Interface Not Equal To", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 != @p1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$ne": 1}}}},
+		{name: "Nested Map Interface Greater than ", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 > @p1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gt": 1}}}},
+		{name: "Nested Map Interface Greater than Equal To", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 >= @p1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gte": 1}}}},
+		{name: "Nested Map Interface Less Than", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 < @p1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lt": 1}}}},
+		{name: "Nested Map Interface Less Than Equal To", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 <= @p1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lte": 1}}}},
+		{name: "Nested Map Interface In", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 IN (@p1))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$in": 1}}}},
+		{name: "Nested Map Interface Not in", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 NOT IN (@p1))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$nin": 1}}}},
+		{name: "Nested Map Interface OR", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE ((string1ofstring1 = @p1) OR (string1ofstring2 = @p2))", req: model.DeleteRequest{Find: map[string]interface{}{"$or": []interface{}{map[string]interface{}{"string1ofstring1": "1"}, map[string]interface{}{"string1ofstring2": "2"}}}}},
+		{name: "When length is 0", dbType: "sqlserver", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable", req: model.DeleteRequest{Find: map[string]interface{}{}}},
+
+		{name: "Successfull Test", dbType: "postgres", tableName: "fooTable", orThis: "DELETE FROM projectName.fooTable WHERE ((String2 = $1) AND (String1 = $2))", wantThis: "DELETE FROM projectName.fooTable WHERE ((String1 = $1) AND (String2 = $2))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": "1", "String2": "2"}}},
+		{name: "Nested Map Interface Equal To", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 = $1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$eq": 1}}}},
+		{name: "Nested Map Interface Not Equal To", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 != $1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$ne": 1}}}},
+		{name: "Nested Map Interface Greater than ", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 > $1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gt": 1}}}},
+		{name: "Nested Map Interface Greater than Equal To", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 >= $1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$gte": 1}}}},
+		{name: "Nested Map Interface Less Than", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 < $1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lt": 1}}}},
+		{name: "Nested Map Interface Less Than Equal To", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 <= $1)", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$lte": 1}}}},
+		{name: "Nested Map Interface In", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 IN ($1))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$in": 1}}}},
+		{name: "Nested Map Interface Not in", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE (String1 NOT IN ($1))", req: model.DeleteRequest{Find: map[string]interface{}{"String1": map[string]interface{}{"$nin": 1}}}},
+		{name: "Nested Map Interface OR", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable WHERE ((string1ofstring1 = $1) OR (string1ofstring2 = $2))", req: model.DeleteRequest{Find: map[string]interface{}{"$or": []interface{}{map[string]interface{}{"string1ofstring1": "1"}, map[string]interface{}{"string1ofstring2": "2"}}}}},
+		{name: "When length is 0", dbType: "postgres", tableName: "fooTable", wantThis: "DELETE FROM projectName.fooTable", req: model.DeleteRequest{Find: map[string]interface{}{}}},
 	}
-	var ctx context.Context
 	project := "projectName"
-	s := SQL{dbType: string("sql-mysql")}
+
 	for i, test := range tests {
+		s := &SQL{dbType: test.dbType}
 		t.Run(test.name, func(t *testing.T) {
-			sqlString, _, err := s.generateDeleteQuery(ctx, project, test.tableName, &test.req)
+			sqlString, _, err := s.generateDeleteQuery(project, test.tableName, &test.req)
 			if i == 0 {
 				if ((sqlString != test.wantThis) && (sqlString != test.orThis)) || err != nil {
 					t.Errorf("|Got| %s |But wantThis| %s |But orThis| %s \n %v", sqlString, test.wantThis, test.orThis, err)
