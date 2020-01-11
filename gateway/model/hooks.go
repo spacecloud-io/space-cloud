61cf3851d898b1141b00e77fc5d9aec36edd5d05
sharadregoti <sharadregoti15@gmail.com>
2020-01-11 17:00:10 +0530

    fixed filestore module

--- gateway/model/hooks.go
+++ gateway/model/hooks.go
@@ -35,6 +35,6 @@ type CrudHooks struct {
 
 type EventingModule interface {
 	CreateFileIntentHook(ctx context.Context, req *CreateFileRequest) (*EventIntent, error)
-	DeleteFileIntentHook(ctx context.Context, path string) (*EventIntent, error)
+	DeleteFileIntentHook(ctx context.Context, path string, meta map[string]interface{}) (*EventIntent, error)
 	HookStage(ctx context.Context, intent *EventIntent, err error)
 }
