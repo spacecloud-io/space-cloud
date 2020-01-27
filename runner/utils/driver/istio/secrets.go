bd7f844c319f841a68abb342eb534fed408f25eb
you-know-me <twinkle99@rocketmail.com>
2020-01-27 18:10:39 +0530

    made changes according to the review again

--- runner/utils/driver/istio/secrets.go
+++ runner/utils/driver/istio/secrets.go
@@ -1,16 +1,16 @@
 package istio
 
 import (
-	"fmt"
-
 	b64 "encoding/base64"
 	"encoding/json"
+	"errors"
+	"fmt"
 
+	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	kubeErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/sirupsen/logrus"
 	"github.com/spaceuptech/space-cloud/runner/model"
 )
 
@@ -25,21 +25,23 @@ func (i *Istio) CreateSecret(projectID string, secretObj *model.Secret) error {
 	if kubeErrors.IsNotFound(err) {
 		// Create a new Secret
 		logrus.Debugf("Creating secret (%s)", secretObj.Name)
-		newSecret := generateSecret(projectID, secretObj)
-		_, err := i.kube.CoreV1().Secrets(projectID).Create(newSecret)
+		newSecret, err := generateSecret(projectID, secretObj)
 		if err != nil {
 			return err
 		}
 
+		_, err = i.kube.CoreV1().Secrets(projectID).Create(newSecret)
+		return err
+
 	} else if err == nil {
 		// secret already exists...update it!
 		logrus.Debugf("Updating secret (%s)", secretObj.Name)
-		newSecret := generateSecret(projectID, secretObj)
-		_, err = i.kube.CoreV1().Secrets(projectID).Update(newSecret)
+		newSecret, err := generateSecret(projectID, secretObj)
 		if err != nil {
 			return err
 		}
-		return nil
+		_, err = i.kube.CoreV1().Secrets(projectID).Update(newSecret)
+		return err
 	}
 	logrus.Errorf("Failed to create secret (%s) - %s", secretObj.Name, err)
 	return err
@@ -47,13 +49,14 @@ func (i *Istio) CreateSecret(projectID string, secretObj *model.Secret) error {
 
 // ListSecrets lists all the secrets in the provided name-space!
 func (i *Istio) ListSecrets(projectID string) ([]*model.Secret, error) {
-
+	// List all secrets
 	kubeSecret, err := i.kube.CoreV1().Secrets(projectID).List(metav1.ListOptions{})
 	if err != nil {
 		logrus.Errorf("Failed to fetch list of secrets - %s", err)
 		return nil, err
 	}
 	listOfSecrets := make([]*model.Secret, len(kubeSecret.Items))
+
 	// Modifying SecretValue with empty []byte
 	for i, v := range kubeSecret.Items {
 		s := &model.Secret{
@@ -86,32 +89,30 @@ func (i *Istio) SetKey(projectID string, secretName string, secretKey string, se
 		logrus.Errorf("Empty key/value provided. Key not set")
 		return fmt.Errorf("key/value not provided; got (%s,%s)", secretName, secretValObj.Value)
 	}
-	// encoding secret value to base64
+	// Encoding secret value to base64
 	encSecret := b64.StdEncoding.EncodeToString([]byte(secretValObj.Value))
-	//Get secret and then check type
+
+	// Get secret and then check type
 	kubeSecret, err := i.kube.CoreV1().Secrets(projectID).Get(secretName, metav1.GetOptions{})
 
 	if kubeErrors.IsNotFound(err) {
 		return err
 	} else if err == nil {
-		//Add secret key-value
+		// Add secret key-value
 		switch kubeSecret.Type {
 		case v1.SecretTypeDockerConfigJson:
-			return fmt.Errorf("setKey operation cannot be performed on secrets with type docker")
+			return fmt.Errorf("set key operation cannot be performed on secrets with type docker")
 		case v1.SecretTypeOpaque:
 			kubeSecret.Data[secretKey] = []byte(encSecret)
 		default:
-			//Throw error
 			return fmt.Errorf("invalid secret type - %s", kubeSecret.Type)
 		}
 
-		//Update the secret
+		// Update the secret
 		_, err := i.kube.CoreV1().Secrets(projectID).Update(kubeSecret)
-		if err != nil {
-			return err
-		}
+		return err
 	}
-	return nil
+	return err
 }
 
 // DeleteKey is used to delete a key from the secret!
@@ -122,46 +123,53 @@ func (i *Istio) DeleteKey(projectID string, secretName string, secretKey string)
 	if kubeErrors.IsNotFound(err) {
 		return fmt.Errorf("secret with name (%s) does not exist- %s", secretName, err)
 	} else if err == nil {
-		// check the type of secret (docker/opaque)
+		// Check the type of secret (docker/opaque)
 		switch kubeSecret.Type {
 		case v1.SecretTypeDockerConfigJson:
-			return fmt.Errorf("deleteKey operation cannot be performed on secrets with type docker")
+			return fmt.Errorf("delete key operation cannot be performed on secrets with type docker")
 		case v1.SecretTypeOpaque:
 			delete(kubeSecret.Data, secretKey)
 		default:
-			// Throw error
 			return fmt.Errorf("invalid secret type - %s", kubeSecret.Type)
 		}
+
 		// Update the secret
 		_, err := i.kube.CoreV1().Secrets(projectID).Update(kubeSecret)
-		if err != nil {
-			return err
-		}
+		return err
 	}
-	return nil
+	return err
 }
 
 // helper function
-func generateSecret(projectID string, secret *model.Secret) *v1.Secret {
+func generateSecret(projectID string, secret *model.Secret) (*v1.Secret, error) {
 	encodedData := map[string][]byte{}
 	var typeOfSecret v1.SecretType
 
 	// Check what type of secret is to be created: file/env/docker
-	if secret.Type == model.FileType || secret.Type == model.EnvType {
+	switch secret.Type {
+	case model.FileType, model.EnvType:
 		typeOfSecret = v1.SecretTypeOpaque
 		// Base64 encoding!
 		for k, v := range secret.Data {
 			encValue := b64.StdEncoding.EncodeToString([]byte(v))
 			encodedData[k] = []byte(encValue)
 		}
-	} else if secret.Type == model.DockerType {
+	case model.DockerType:
+		username, p1 := secret.Data["username"]
+		password, p2 := secret.Data["password"]
+		url, p3 := secret.Data["url"]
+
+		if !p1 || !p2 || !p3 {
+			return nil, errors.New("incorrect secret value provided for secret type docker")
+		}
+
 		typeOfSecret = v1.SecretTypeDockerConfigJson
-		authSecret := secret.Data["username"] + ":" + secret.Data["password"]
+		authSecret := username + ":" + password
 		encAuthSecret := b64.StdEncoding.EncodeToString([]byte(authSecret))
 		// ref: https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/#registry-secret-existing-credentials
 		dockerJSON := map[string]interface{}{
 			"auths": map[string]interface{}{
-				secret.Data["url"]: map[string]string{
+				url: map[string]string{
 					"auth": encAuthSecret,
 				},
 			},
@@ -170,6 +178,16 @@ func generateSecret(projectID string, secret *model.Secret) *v1.Secret {
 		// encode the entire json object in base64 encoding
 		encValue := b64.StdEncoding.EncodeToString([]byte(data))
 		encodedData[v1.DockerConfigJsonKey] = []byte(encValue)
+	default:
+		return nil, fmt.Errorf("invalid secret type (%s) provided", secret.Type)
 	}
-	return &v1.Secret{Type: typeOfSecret, ObjectMeta: metav1.ObjectMeta{Name: secret.Name, Namespace: projectID, Annotations: map[string]string{"rootPath": secret.RootPath, "secretType": secret.Type}}, Data: encodedData}
+	return &v1.Secret{
+		Type: typeOfSecret,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        secret.Name,
+			Namespace:   projectID,
+			Annotations: map[string]string{"rootPath": secret.RootPath, "secretType": secret.Type},
+		},
+		Data: encodedData,
+	}, nil
 }
