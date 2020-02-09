c66860d23d5e403084bfbba22d3c766fe0dd726f
Kavish Shah <kavishshah09@gmail.com>
2020-02-09 08:13:02 +0530

    Added linter support to runner and modified github workflow for runner (#711)

--- runner/utils/driver/docker/helpers.go
+++ runner/utils/driver/docker/helpers.go
@@ -19,12 +19,12 @@ func getServiceDomain(projectID, serviceID string) string {
 	return fmt.Sprintf("%s.%s.svc.cluster.local", serviceID, projectID)
 }
 
-func (d *docker) pullImageIfDoesntExists(ctx context.Context, projectId string, taskDocker model.Docker) error {
+func (d *Docker) pullImageIfDoesntExists(ctx context.Context, projectID string, taskDocker model.Docker) error {
 	_, _, err := d.client.ImageInspectWithRaw(ctx, taskDocker.Image)
 	if err != nil {
 		// image doesn't exist locally
 		if taskDocker.Secret != "" {
-			data, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/%s.json", d.secretPath, projectId, taskDocker.Secret))
+			data, err := ioutil.ReadFile(fmt.Sprintf("%s/%s/%s.json", d.secretPath, projectID, taskDocker.Secret))
 			if err != nil {
 				logrus.Errorf("error in docker unable to read file name (%s) required for pulling image from private repository - %v", taskDocker.Secret, err.Error())
 				return err
@@ -49,7 +49,7 @@ func (d *docker) pullImageIfDoesntExists(ctx context.Context, projectId string, 
 				logrus.Errorf("error in docker unable to pull private image with id (%s) - %s", taskDocker.Image, err.Error())
 				return err
 			}
-			io.Copy(os.Stdout, out)
+			_, _ = io.Copy(os.Stdout, out)
 		} else {
 			// pull image from public repository
 			out, err := d.client.ImagePull(ctx, taskDocker.Image, types.ImagePullOptions{})
@@ -57,7 +57,7 @@ func (d *docker) pullImageIfDoesntExists(ctx context.Context, projectId string, 
 				logrus.Errorf("error in docker unable to pull public image with id (%s) - %s", taskDocker.Image, err.Error())
 				return err
 			}
-			io.Copy(os.Stdout, out)
+			_, _ = io.Copy(os.Stdout, out)
 		}
 	}
 	return nil
