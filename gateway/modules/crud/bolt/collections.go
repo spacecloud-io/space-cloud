45c156da46f41eee151eba351d84cdaf2848b586
Kavish Shah <kavishshah09@gmail.com>
2020-02-15 11:38:06 +0530

    implemented get collections method for embedded db

--- gateway/modules/crud/bolt/collections.go
+++ gateway/modules/crud/bolt/collections.go
@@ -3,13 +3,35 @@ package bolt
 import (
 	"context"
 	"fmt"
+	"strings"
+
+	"go.etcd.io/bbolt"
 
 	"github.com/spaceuptech/space-cloud/gateway/utils"
 )
 
 // GetCollections returns collection / tables name of specified database
 func (b *Bolt) GetCollections(ctx context.Context, project string) ([]utils.DatabaseCollections, error) {
-	return nil, fmt.Errorf("error getting collection operation not supported for selected database")
+	keys := make(map[string]bool)
+	err := b.client.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(project))
+
+		c := b.Cursor()
+
+		for key, _ := c.First(); key != nil; key, _ = c.Next() {
+			keys[strings.Split(string(key), "/")[0]] = true
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not get all collections for given project and database")
+	}
+	dbCols := make([]utils.DatabaseCollections, len(keys))
+	for col := range keys {
+		dbCols = append(dbCols, utils.DatabaseCollections{TableName: col})
+	}
+	return dbCols, nil
 }
 
 // DeleteCollection deletes collection / tables name of specified database
