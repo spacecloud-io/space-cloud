044770f7b42f067de110e861f07f779610d15952
Kavish Shah <kavishshah09@gmail.com>
2020-03-06 17:42:51 +0530

    eventing set config now sets log tables (#804)
    Fixes #730

---
+++ gateway/modules/eventing/types.go
@@ -0,0 +1,31 @@
+package eventing
+
+const (
+	eventingLogs     string = "event_logs"
+	invocationLogs   string = "invocation_logs"
+	invocationSchema string = `type invocation_logs {
+		_id: ID! @primary
+		event_id: ID! @foreign(table: "event_logs", field: "_id")
+		invocation_time: DateTime!
+		request_payload: String
+		response_status_code: Integer
+		response_body: String
+		error_msg: String
+		remark: String	
+	  }`
+	eventSchema string = `type event_logs {
+		_id: ID! @primary
+		batchid: String
+		type: String
+		rule_name: String
+		token: Integer
+		timestamp: Integer
+		event_timestamp: Integer
+		payload: String
+		status: String
+		retries: Integer
+		url: String
+		remark: String
+		invocations: [invocation_logs]! @link(table: "invocation_logs", from: "_id", to: "event_id")
+	  }`
+)
