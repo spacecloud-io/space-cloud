7d65d50a699fa3741659d433d8823151230adb1d
Kavish Shah <kavishshah09@gmail.com>
2020-03-19 15:32:39 +0530

    cli remove registry

--- space-cli/modules/addons/actions.go
+++ space-cli/modules/addons/actions.go
@@ -19,10 +19,28 @@ var Commands = []cli.Command{
 			},
 		},
 	},
+	{
+		Name:  "remove",
+		Usage: "Remove a add-on from the environment",
+		Flags: []cli.Flag{cli.StringFlag{Name: "project", Usage: "The project to remove the add-on from"}},
+		Subcommands: []cli.Command{
+			{
+				Name:   "registry",
+				Usage:  "Remove a docker registry",
+				Action: ActionRemoveRegistry,
+			},
+		},
+	},
 }
 
 // ActionAddRegistry adds a registry add on
 func ActionAddRegistry(c *cli.Context) error {
 	project := c.GlobalString("project")
 	return addRegistry(project)
+}
+
+// ActionRemoveRegistry removes a registry add on
+func ActionRemoveRegistry(c *cli.Context) error {
+	project := c.GlobalString("project")
+	return removeRegistry(project)
 }
