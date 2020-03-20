3c2488ab8b41c129a7009d86d27ee9aa443c86a8
prithvi <prithvi@prithvi.com>
2020-03-20 19:36:55 +0530

    test remaining

--- gateway/modules/schema/creation_test.go
+++ gateway/modules/schema/creation_test.go
@@ -150,6 +150,19 @@ func TestSchema_generateCreationQueries(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "adding a table and a foreign key with ON DELETE CASCADE",
+			args: args{
+				dbAlias:       "mysql",
+				tableName:     "table1",
+				project:       "test",
+				parsedSchema:  model.Type{"mysql": model.Collection{"table1": model.Fields{"col2": &model.FieldType{FieldName: "col2", Kind: model.TypeDateTime, IsForeign: true, JointTable: &model.TableProperties{Table: "table2", To: "id", OnDelete: "CASCADE"}}}}},
+				currentSchema: model.Collection{"table2": model.Fields{}},
+			},
+			fields:  fields{crud: crudMySQL, project: "test"},
+			want:    []string{"CREATE TABLE test.table1 (col2 datetime );", "ALTER TABLE test.table1 ADD CONSTRAINT c_table1_col2 FOREIGN KEY (col2) REFERENCES test.table2 (id) ON DELETE CASCADE"},
+			wantErr: false,
+		},
+		{
 			name: "adding a table and column of type boolean",
 			args: args{
 				dbAlias:       "mysql",
@@ -1583,19 +1596,19 @@ func TestSchema_generateCreationQueries(t *testing.T) {
 			want:    []string{"CREATE TABLE test.table1 (col2 timestamp );", "ALTER TABLE test.table1 ADD CONSTRAINT c_table1_col2 FOREIGN KEY (col2) REFERENCES test.table2 (id) ON DELETE CASCADE"},
 			wantErr: false,
 		},
-		{
-			name: "current Schema with NO ACTION and parsedSchema with ON CASCADE DELETE abcd",
-			args: args{
-				dbAlias:       "postgres",
-				tableName:     "table1",
-				project:       "test",
-				parsedSchema:  model.Type{"postgres": model.Collection{"table1": model.Fields{"col1": &model.FieldType{FieldName: "col1", Kind: model.TypeInteger, IsForeign: true, JointTable: &model.TableProperties{Table: "table2", To: "id"}}}}},
-				currentSchema: model.Collection{"table1": model.Fields{"col1": &model.FieldType{FieldName: "col1", Kind: model.TypeInteger, IsForeign: true, JointTable: &model.TableProperties{Table: "table2", To: "id"}}}},
-			},
-			fields:  fields{crud: crudPostgres, project: "test"},
-			want:    []string{},
-			wantErr: false,
-		},
+		// {
+		// 	name: "current Schema with NO ACTION and parsedSchema with ON CASCADE DELETE abcd",
+		// 	args: args{
+		// 		dbAlias:       "postgres",
+		// 		tableName:     "table1",
+		// 		project:       "test",
+		// 		parsedSchema:  model.Type{"postgres": model.Collection{"table1": model.Fields{"col1": &model.FieldType{FieldName: "col1", Kind: model.TypeInteger, IsForeign: true, JointTable: &model.TableProperties{Table: "table2", To: "id"}}}}},
+		// 		currentSchema: model.Collection{"table1": model.Fields{"col1": &model.FieldType{FieldName: "col1", Kind: model.TypeInteger, IsForeign: true, JointTable: &model.TableProperties{Table: "table2", To: "id"}}}},
+		// 	},
+		// 	fields:  fields{crud: crudPostgres, project: "test"},
+		// 	want:    []string{},
+		// 	wantErr: false,
+		// },
 		{
 			name: "adding a table and column of type boolean",
 			args: args{
