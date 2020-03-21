4a2fb825324e1eea1ba1a78ee725b89e2013ba74
Kavish Shah <kavishshah09@gmail.com>
2020-03-21 15:37:26 +0530

    removed module and segments in logs

--- space-cli/modules/addons/database.go
+++ space-cli/modules/addons/database.go
@@ -19,23 +19,23 @@ func addDatabase(dbtype, username, password, alias, version string) error {
 	// Create a docker client
 	docker, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		return utils.LogError("Unable to initialize docker client", "add", "database", err)
+		return utils.LogError("Unable to initialize docker client", err)
 	}
 
 	// Check if a database container already exist
 	filterArgs := filters.Arg("label", "app=space-cloud")
 	containers, err := docker.ContainerList(ctx, types.ContainerListOptions{Filters: filters.NewArgs(filterArgs)})
 	if err != nil {
-		return utils.LogError("Unable to check if database already exists", "add", "database", err)
+		return utils.LogError("Unable to check if database already exists", err)
 	}
 	if len(containers) == 0 {
-		utils.LogInfo("No space-cloud instance found. Run 'space-cli setup' first", "add", "database")
+		utils.LogInfo("No space-cloud instance found. Run 'space-cli setup' first")
 		return nil
 	}
 
 	// Pull image if it doesn't already exist
 	if err := utils.PullImageIfNotExist(ctx, docker, dockerImage); err != nil {
-		return utils.LogError(fmt.Sprintf("Could not pull the image (%s). Make sure docker is running and that you have an active internet connection.", dockerImage), "add", "database", err)
+		return utils.LogError(fmt.Sprintf("Could not pull the image (%s). Make sure docker is running and that you have an active internet connection.", dockerImage), err)
 	}
 
 	// Create the database
@@ -46,30 +46,30 @@ func addDatabase(dbtype, username, password, alias, version string) error {
 		NetworkMode: "space-cloud",
 	}, nil, fmt.Sprintf("space-cloud--addon--db--%s", alias))
 	if err != nil {
-		return utils.LogError("Unable to create local docker database", "add", "database", err)
+		return utils.LogError("Unable to create local docker database", err)
 	}
 
 	// Start the database
 	if err := docker.ContainerStart(ctx, containerRes.ID, types.ContainerStartOptions{}); err != nil {
-		return utils.LogError("Unable to start local docker database", "add", "database", err)
+		return utils.LogError("Unable to start local docker database", err)
 	}
 
 	// Get the hosts file
 	hosts, err := txeh.NewHosts(&txeh.HostsConfig{ReadFilePath: utils.GetSpaceCloudHostsFilePath(), WriteFilePath: utils.GetSpaceCloudHostsFilePath()})
 	if err != nil {
-		return utils.LogError("Unable to open hosts file", "add", "database", err)
+		return utils.LogError("Unable to open hosts file", err)
 	}
 
 	for _, container := range containers {
 		// First start the container
 		if err := docker.ContainerStart(ctx, container.ID, types.ContainerStartOptions{}); err != nil {
-			return utils.LogError(fmt.Sprintf("Unable to start container (%s)", container.ID), "add", "database", err)
+			return utils.LogError(fmt.Sprintf("Unable to start container (%s)", container.ID), err)
 		}
 
 		// Get the container's info
 		info, err := docker.ContainerInspect(ctx, container.ID)
 		if err != nil {
-			return utils.LogError(fmt.Sprintf("Unable to inspect container (%s)", container.ID), "add", "database", err)
+			return utils.LogError(fmt.Sprintf("Unable to inspect container (%s)", container.ID), err)
 		}
 
 		hostName := utils.GetServiceDomain(info.Config.Labels["service"], info.Config.Labels["name"])
@@ -83,7 +83,7 @@ func addDatabase(dbtype, username, password, alias, version string) error {
 
 	// Save the hosts file
 	if err := hosts.Save(); err != nil {
-		return utils.LogError("Could not save hosts file after updating add on containers", "add", "database", err)
+		return utils.LogError("Could not save hosts file after updating add on containers", err)
 	}
 
 	return nil
@@ -95,24 +95,24 @@ func removeDatabase(alias string) error {
 	// Create a docker client
 	docker, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		return utils.LogError("Unable to initialize docker client", "remove", "database", err)
+		return utils.LogError("Unable to initialize docker client", err)
 	}
 
 	// Check if a database container already exist
 	filterArgs := filters.Arg("label", fmt.Sprintf("space-cloud--addon--db--%s", alias))
 	containers, err := docker.ContainerList(ctx, types.ContainerListOptions{Filters: filters.NewArgs(filterArgs)})
 	if err != nil {
-		return utils.LogError("Unable to check if database already exists", "remove", "database", err)
+		return utils.LogError("Unable to check if database already exists", err)
 	}
 	if len(containers) == 0 {
-		utils.LogInfo(fmt.Sprintf("Database (%s) not found.", alias), "remove", "database")
+		utils.LogInfo(fmt.Sprintf("Database (%s) not found.", alias))
 		return nil
 	}
 
 	// Get the hosts file
 	hosts, err := txeh.NewHosts(&txeh.HostsConfig{ReadFilePath: utils.GetSpaceCloudHostsFilePath(), WriteFilePath: utils.GetSpaceCloudHostsFilePath()})
 	if err != nil {
-		return utils.LogError("Unable to open hosts file", "remove", "database", err)
+		return utils.LogError("Unable to open hosts file", err)
 	}
 
 	for _, container := range containers {
@@ -123,13 +123,13 @@ func removeDatabase(alias string) error {
 
 		// remove the container from host machine
 		if err := docker.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{Force: true}); err != nil {
-			return utils.LogError(fmt.Sprintf("Unable to remove container %s", container.ID), "remove", "registry", err)
+			return utils.LogError(fmt.Sprintf("Unable to remove container %s", container.ID), err)
 		}
 	}
 
 	// Save the hosts file
 	if err := hosts.Save(); err != nil {
-		return utils.LogError("Could not save hosts file after updating add on containers", "add", "database", err)
+		return utils.LogError("Could not save hosts file after updating add on containers", err)
 	}
 
 	return nil
