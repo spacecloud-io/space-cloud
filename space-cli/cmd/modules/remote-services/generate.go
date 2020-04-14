350b4e43f10aad4005a29c47a392a62f01f0b6af
Noorain Panjwani <noorain@spaceuptech.com>
2020-04-14 15:50:40 +0530

    tested & fixed bugs in space cli getters & generators (#937)

--- space-cli/cmd/modules/remote-services/generate.go
+++ space-cli/cmd/modules/remote-services/generate.go
@@ -1,7 +1,10 @@
 package remoteservices
 
 import (
+	"strings"
+
 	"github.com/AlecAivazis/survey/v2"
+
 	"github.com/spaceuptech/space-cli/cmd/model"
 )
 
@@ -11,14 +14,37 @@ func generateService() (*model.SpecObject, error) {
 		return nil, err
 	}
 	service := ""
-	if err := survey.AskOne(&survey.Input{Message: "Enter service: "}, &service); err != nil {
+	if err := survey.AskOne(&survey.Input{Message: "Enter Service Name: "}, &service); err != nil {
 		return nil, err
 	}
 	url := ""
-	if err := survey.AskOne(&survey.Input{Message: "Enter url: "}, &url); err != nil {
+	if err := survey.AskOne(&survey.Input{Message: "Enter Service URL: ", Help: "e.g -> http://localhost:8090"}, &url); err != nil {
 		return nil, err
 	}
-	var endpoint interface{}
+	endpoints := []interface{}{}
+	want := "y"
+	for {
+		endpointName := ""
+		if err := survey.AskOne(&survey.Input{Message: "Enter Endpoint Name: "}, &endpointName); err != nil {
+			return nil, err
+		}
+		methods := ""
+		if err := survey.AskOne(&survey.Select{Message: "Select Method: ", Options: []string{"POST", "PUT", "GET", "DELETE"}}, &methods); err != nil {
+			return nil, err
+		}
+
+		path := ""
+		if err := survey.AskOne(&survey.Input{Message: "Enter URL Path:", Default: "/"}, &path); err != nil {
+			return nil, err
+		}
+		endpoints = append(endpoints, map[string]interface{}{endpointName: map[string]interface{}{"method": methods, "path": path}})
+		if err := survey.AskOne(&survey.Input{Message: "Add another host?(Y/n)", Default: "n"}, &want); err != nil {
+			return nil, err
+		}
+		if strings.ToLower(want) == "n" {
+			break
+		}
+	}
 	v := &model.SpecObject{
 		API:  "/v1/config/projects/{project}/remote-service/service/{id}",
 		Type: "remote-services",
@@ -27,8 +53,8 @@ func generateService() (*model.SpecObject, error) {
 			"project": project,
 		},
 		Spec: map[string]interface{}{
-			"URL":       url,
-			"Endpoints": endpoint,
+			"url":       url,
+			"endpoints": endpoints,
 		},
 	}
 
