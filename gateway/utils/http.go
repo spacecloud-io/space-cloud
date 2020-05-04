f7663a969a3970e5fa0733300fdf0b1ddb90820e
prithvi <prithvi@prithvi.com>
2020-05-04 13:56:18 +0530

    added cache control

--- gateway/utils/http.go
+++ gateway/utils/http.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/rs/cors"
-	"github.com/sirupsen/logrus"
 )
 
 // GetTokenFromHeader returns the token from the request header
@@ -39,10 +38,21 @@ func CloseTheCloser(c io.Closer) {
 	_ = c.Close()
 }
 
-// SendErrorResponse sends an error http response
-func SendErrorResponse(w http.ResponseWriter, r *http.Request, status int, err error) {
+// SendOkayResponse sends an Okay http response
+func SendOkayResponse(w http.ResponseWriter, r *http.Request) error {
+	return SendResponse(w, r, 200, map[string]string{})
+}
+
+// SendErrorResponse sends an Error http response
+func SendErrorResponse(w http.ResponseWriter, r *http.Request, status int, err error) error {
+	return SendResponse(w, r, status, map[string]string{"error": err.Error()})
+}
+
+// SendResponse sends an http response
+func SendResponse(w http.ResponseWriter, r *http.Request, status int, body interface{}) error {
+	w.Header().Set("Cache-Control", "no-store")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	if err := json.NewEncoder(w).Encode(map[string]interface{}{"error": err.Error()}); err != nil {
-		logrus.Errorf("Error while sending error response for %s %s - %s", r.Method, r.URL.String(), err.Error())
-	}
+	err := json.NewEncoder(w).Encode(body)
+	return err
 }
