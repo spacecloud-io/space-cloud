59e9117d7d5a3e34f54482eb9e6d7060b20c21b7
prithvi081099 <47061269+prithvi081099@users.noreply.github.com>
2020-05-09 21:02:22 +0530

    added format error (#964)
    Fixes #963

--- gateway/modules/auth/errors.go
+++ gateway/modules/auth/errors.go
@@ -1,6 +1,11 @@
 package auth
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spaceuptech/space-cloud/gateway/config"
+)
 
 // ErrRuleNotFound is thrown when an error is not present in the auth object
 var ErrRuleNotFound = errors.New("auth: No rule has been provided")
@@ -10,3 +15,19 @@ var ErrIncorrectRuleFieldType = errors.New("auth: Incorrect rule field type")
 
 // ErrIncorrectMatch is thrown when the field type of a rule is of incorrect type
 var ErrIncorrectMatch = errors.New("auth: The two fields do not match")
+
+//Formaterror check whether error is provided in config.Rule
+func formatError(rule *config.Rule, err error) error {
+	if err == nil {
+		return nil
+	}
+	if rule.Name == "" {
+		rule.Name = "no name"
+	}
+	logrus.Errorf("Rule (%s) of type (%s) failed", rule.Name, rule.Rule)
+
+	if rule.Error == "" {
+		return err
+	}
+	return errors.New(rule.Error)
+}
