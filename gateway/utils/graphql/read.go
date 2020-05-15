23be83d57b9460d8d196714c79cedc800936c823
Master <sharadregoti15@gmail.com>
2020-05-15 20:44:04 +0530

    added support for aggregation for following functions (#983)

--- gateway/utils/graphql/read.go
+++ gateway/utils/graphql/read.go
@@ -51,6 +51,11 @@ func (graph *Module) execReadRequest(ctx context.Context, field *ast.Field, toke
 		return
 	}
 
+	selectionSet := graph.extractSelectionSet(field, dbAlias, col)
+	if len(selectionSet) > 0 {
+		req.Options.Select = selectionSet
+	}
+
 	// Check if read op is authorised
 	actions, _, err := graph.auth.IsReadOpAuthorised(ctx, graph.project, dbAlias, col, token, req)
 	if err != nil {
@@ -59,7 +64,8 @@ func (graph *Module) execReadRequest(ctx context.Context, field *ast.Field, toke
 	}
 
 	go func() {
-		req.IsBatch = true
+		//  batch operation cannot be performed with aggregation
+		req.IsBatch = !(len(req.Aggregate) > 0)
 		req.Options.HasOptions = hasOptions
 		result, err := graph.crud.Read(ctx, dbAlias, col, req)
 		_ = graph.auth.PostProcessMethod(actions, result)
@@ -107,6 +113,16 @@ func generateReadRequest(field *ast.Field, store utils.M) (*model.ReadRequest, b
 		return nil, false, err
 	}
 
+	readRequest.GroupBy, err = extractGroupByClause(field.Arguments, store)
+	if err != nil {
+		return nil, false, err
+	}
+
+	readRequest.Aggregate, err = extractAggregate(field)
+	if err != nil {
+		return nil, false, err
+	}
+
 	var hasOptions bool
 	readRequest.Options, hasOptions, err = generateOptions(field.Arguments, store)
 	if err != nil {
@@ -118,6 +134,82 @@ func generateReadRequest(field *ast.Field, store utils.M) (*model.ReadRequest, b
 	}
 
 	return &readRequest, hasOptions, nil
+}
+
+func (graph *Module) extractSelectionSet(field *ast.Field, dbAlias, col string) map[string]int32 {
+	selectMap := map[string]int32{}
+	schemaFields, _ := graph.schema.GetSchema(dbAlias, col)
+	if field.SelectionSet == nil {
+		return nil
+	}
+	for _, selection := range field.SelectionSet.Selections {
+		v := selection.(*ast.Field)
+		// skip aggregate field & fields with directives
+		if v.Name.Value == utils.GraphQLAggregate || len(v.Directives) > 0 {
+			continue
+		}
+		if schemaFields != nil {
+			// skip linked fields
+			fieldStruct, p := schemaFields[v.Name.Value]
+			if p && fieldStruct.IsLinked {
+				continue
+			}
+		}
+		selectMap[v.Name.Value] = 1
+	}
+	return selectMap
+}
+
+func extractAggregate(v *ast.Field) (map[string][]string, error) {
+	functionMap := make(map[string][]string)
+	aggregateFound := false
+	if v.SelectionSet == nil {
+		return nil, nil
+	}
+	for _, selection := range v.SelectionSet.Selections {
+		field := selection.(*ast.Field)
+		if field.Name.Value != utils.GraphQLAggregate || field.SelectionSet == nil {
+			continue
+		}
+		if aggregateFound {
+			return nil, utils.LogError("GraphQL query cannot have multiple aggregate fields, specify all functions in single aggregate field", nil)
+		}
+		aggregateFound = true
+		// get function name
+		for _, selection := range field.SelectionSet.Selections {
+			functionField := selection.(*ast.Field)
+			_, ok := functionMap[functionField.Name.Value]
+			if ok {
+				return nil, utils.LogError(fmt.Sprintf("Cannot repeat the same function (%s) twice. Specify all columns within single function field", functionField.Name.Value), nil)
+			}
+			colArray := make([]string, 0)
+			// get column name
+			for _, selection := range functionField.SelectionSet.Selections {
+				columnField := selection.(*ast.Field)
+				colArray = append(colArray, columnField.Name.Value)
+			}
+			functionMap[functionField.Name.Value] = colArray
+		}
+	}
+	return functionMap, nil
+}
+
+func extractGroupByClause(args []*ast.Argument, store utils.M) ([]interface{}, error) {
+	for _, v := range args {
+		switch v.Name.Value {
+		case utils.GraphQLGroupByArgument:
+			temp, err := utils.ParseGraphqlValue(v.Value, store)
+			if err != nil {
+				return nil, err
+			}
+			if obj, ok := temp.([]interface{}); ok {
+				return obj, nil
+			}
+			return nil, utils.LogError(fmt.Sprintf("GraphQL (%s) argument is of type %v, but it should be of type array ([])", utils.GraphQLGroupByArgument, reflect.TypeOf(temp)), nil)
+		}
+	}
+
+	return make([]interface{}, 0), nil
 }
 
 // ExtractWhereClause return the where arg of graphql schema
