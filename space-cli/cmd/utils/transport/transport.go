3b7078544f01a7c6ea5af0707bf5a4554ccb1000
Khannaabhi <abhishekakhanna@gmail.com>
2020-06-10 19:17:13 +0530

    log command for space-cli 1) added log cmd as "space-cli get service-logs" 2) handled error when service not found 3) handle error even in handle_runner.go 4) added getLog function in transport.go

--- space-cli/cmd/utils/transport/transport.go
+++ space-cli/cmd/utils/transport/transport.go
@@ -1,11 +1,13 @@
 package transport
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/spaceuptech/space-cli/cmd/utils"
 	"github.com/stretchr/testify/mock"
@@ -13,6 +15,7 @@ import (
 
 type transport interface {
 	Get(method, url string, params map[string]string, vPtr interface{}) error
+	GetLogs(method, url string) error
 }
 
 type def struct{}
@@ -69,6 +72,50 @@ func (d *def) Get(method, url string, params map[string]string, vPtr interface{}
 	return nil
 }
 
+func (d *def) GetLogs(method, url string) error {
+	account, token, err := utils.LoginWithSelectedAccount()
+	if err != nil {
+		return utils.LogError("Couldn't get account details or login token", err)
+	}
+	url = fmt.Sprintf("%s%s", account.ServerURL, url)
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return err
+	}
+	if token != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer CloseTheCloser(resp.Body)
+
+	if resp.StatusCode != 200 {
+		data, _ := ioutil.ReadAll(resp.Body)
+		respBody := map[string]interface{}{}
+		if err := json.Unmarshal(data, &respBody); err != nil {
+			return err
+		}
+		_ = utils.LogError(fmt.Sprintf("error while getting service logs got error %s", respBody["error"]), nil)
+		return fmt.Errorf("received invalid status code (%d)", resp.StatusCode)
+	}
+
+	rd := bufio.NewReader(resp.Body)
+
+	utils.LogInfo("Press crtl + c to exit")
+	for {
+		str, _ := rd.ReadString('\n')
+		if str != "\n" {
+			fmt.Println(str)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
 // CloseTheCloser closes the closer
 func CloseTheCloser(c io.Closer) {
 	_ = c.Close()
@@ -84,5 +131,11 @@ func (m *MocketAuthProviders) Get(method, url string, params map[string]string, 
 	c := m.Called(method, url, params, vPtr)
 	a, _ := json.Marshal(c[1])
 	_ = json.Unmarshal(a, vPtr)
+	return c.Error(0)
+}
+
+// GetLogs gets logs of service during test
+func (m *MocketAuthProviders) GetLogs(method, url string) error {
+	c := m.Called(method, url)
 	return c.Error(0)
 }
