6fe76879ee571bd72deb029f4a0c7a14d8a79f0f
Kavish Shah <kavishshah09@gmail.com>
2020-06-16 16:08:13 +0530

    syncman_crud.go more progress

--- gateway/utils/syncman/syncman_crud_test.go
+++ gateway/utils/syncman/syncman_crud_test.go
@@ -464,3 +464,212 @@ func TestManager_GetPreparedQuery(t *testing.T) {
 		})
 	}
 }
+
+func TestManager_SetPreparedQueries(t *testing.T) {
+	type mockArgs struct {
+		method         string
+		args           []interface{}
+		paramsReturned []interface{}
+	}
+	type args struct {
+		ctx     context.Context
+		project string
+		dbAlias string
+		id      string
+		v       *config.PreparedQuery
+	}
+	tests := []struct {
+		name            string
+		s               *Manager
+		args            args
+		modulesMockArgs []mockArgs
+		storeMockArgs   []mockArgs
+		wantErr         bool
+	}{
+		{
+			name:    "unable to get project",
+			s:       &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args:    args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "2", v: &config.PreparedQuery{ID: "queryID", SQL: "field"}},
+			wantErr: true,
+		},
+		{
+			name:    "database not present in config",
+			s:       &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args:    args{ctx: context.Background(), dbAlias: "notAlias", id: "id", project: "1", v: &config.PreparedQuery{ID: "queryID", SQL: "field"}},
+			wantErr: true,
+		},
+		{
+			name: "unable to set crud config",
+			s:    &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "1", v: &config.PreparedQuery{ID: "queryID", SQL: "field"}},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{errors.New("unable to set db config")},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "unable to set project",
+			s:    &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "1", v: &config.PreparedQuery{ID: "queryID", SQL: "field"}},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "prepared queries are set",
+			s:    &Manager{storeType: "kube", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "1", v: &config.PreparedQuery{ID: "queryID", SQL: "field"}},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			storeMockArgs: []mockArgs{
+				{
+					method:         "SetProject",
+					args:           []interface{}{context.Background(), mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockModules := mockModulesInterface{}
+			mockStore := mockStoreInterface{}
+
+			for _, m := range tt.modulesMockArgs {
+				mockModules.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+			for _, m := range tt.storeMockArgs {
+				mockStore.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+
+			tt.s.modules = &mockModules
+			tt.s.store = &mockStore
+
+			if err := tt.s.SetPreparedQueries(tt.args.ctx, tt.args.project, tt.args.dbAlias, tt.args.id, tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Manager.SetPreparedQueries() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			mockModules.AssertExpectations(t)
+			mockStore.AssertExpectations(t)
+		})
+	}
+}
+
+func TestManager_RemovePreparedQueries(t *testing.T) {
+	type mockArgs struct {
+		method         string
+		args           []interface{}
+		paramsReturned []interface{}
+	}
+	type args struct {
+		ctx     context.Context
+		project string
+		dbAlias string
+		id      string
+	}
+	tests := []struct {
+		name            string
+		s               *Manager
+		args            args
+		modulesMockArgs []mockArgs
+		storeMockArgs   []mockArgs
+		wantErr         bool
+	}{
+		{
+			name:    "unable to get project",
+			s:       &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args:    args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "2"},
+			wantErr: true,
+		},
+		{
+			name:    "database not present in config",
+			s:       &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args:    args{ctx: context.Background(), dbAlias: "notAlias", id: "id", project: "1"},
+			wantErr: true,
+		},
+		{
+			name: "unable to set crud config",
+			s:    &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "1"},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{errors.New("unable to set db config")},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "unable to set project",
+			s:    &Manager{storeType: "none", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "1"},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "prepared queries is removed",
+			s:    &Manager{storeType: "kube", projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{PreparedQueries: map[string]*config.PreparedQuery{"key": {ID: "id", SQL: "field"}}}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", id: "id", project: "1"},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			storeMockArgs: []mockArgs{
+				{
+					method:         "SetProject",
+					args:           []interface{}{context.Background(), mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockModules := mockModulesInterface{}
+			mockStore := mockStoreInterface{}
+
+			for _, m := range tt.modulesMockArgs {
+				mockModules.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+			for _, m := range tt.storeMockArgs {
+				mockStore.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+
+			tt.s.modules = &mockModules
+			tt.s.store = &mockStore
+
+			if err := tt.s.RemovePreparedQueries(tt.args.ctx, tt.args.project, tt.args.dbAlias, tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("Manager.RemovePreparedQueries() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			mockModules.AssertExpectations(t)
+			mockStore.AssertExpectations(t)
+		})
+	}
+}
