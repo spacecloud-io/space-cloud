2c6fa597d0c52b1d8e82f83c766abc27211f9bde
Kavish Shah <kavishshah09@gmail.com>
2020-06-20 20:06:06 +0530

    setmodifyallschema tests

--- gateway/utils/syncman/syncman_crud_test.go
+++ gateway/utils/syncman/syncman_crud_test.go
@@ -1888,3 +1888,129 @@ func TestManager_applySchemas(t *testing.T) {
 		})
 	}
 }
+
+func TestManager_SetModifyAllSchema(t *testing.T) {
+	mockSchema := mockSchemaEventingInterface{}
+	type mockArgs struct {
+		method         string
+		args           []interface{}
+		paramsReturned []interface{}
+	}
+	type args struct {
+		ctx     context.Context
+		dbAlias string
+		project string
+		v       config.CrudStub
+	}
+	tests := []struct {
+		name            string
+		s               *Manager
+		args            args
+		modulesMockArgs []mockArgs
+		schemaMockArgs  []mockArgs
+		storeMockArgs   []mockArgs
+		wantErr         bool
+	}{
+		{
+			name:    "unable to get project config",
+			s:       &Manager{projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}}}}}}},
+			args:    args{ctx: context.Background(), dbAlias: "alias", project: "2", v: config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}},
+			wantErr: true,
+		},
+		{
+			name:    "unable to apply schemas",
+			s:       &Manager{projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}}}}}}},
+			args:    args{ctx: context.Background(), dbAlias: "notAlias", project: "1", v: config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}},
+			wantErr: true,
+		},
+		{
+			name: "unable to set project",
+			s:    &Manager{projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", project: "1", v: config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "GetSchemaModuleForSyncMan",
+					paramsReturned: []interface{}{&mockSchema},
+				},
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{"1", mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			schemaMockArgs: []mockArgs{
+				{
+					method:         "SchemaModifyAll",
+					args:           []interface{}{context.Background(), "alias", mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			storeMockArgs: []mockArgs{
+				{
+					method:         "SetProject",
+					args:           []interface{}{context.Background(), mock.Anything},
+					paramsReturned: []interface{}{errors.New("unable to get db config")},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "modified all schema successfully",
+			s:    &Manager{projectConfig: &config.Config{Projects: []*config.Project{{ID: "1", Modules: &config.Modules{Crud: config.Crud{"alias": &config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}}}}}}},
+			args: args{ctx: context.Background(), dbAlias: "alias", project: "1", v: config.CrudStub{Collections: map[string]*config.TableRule{"tableName": {}}}},
+			modulesMockArgs: []mockArgs{
+				{
+					method:         "GetSchemaModuleForSyncMan",
+					paramsReturned: []interface{}{&mockSchema},
+				},
+				{
+					method:         "SetCrudConfig",
+					args:           []interface{}{"1", mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			schemaMockArgs: []mockArgs{
+				{
+					method:         "SchemaModifyAll",
+					args:           []interface{}{context.Background(), "alias", mock.Anything, mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+			storeMockArgs: []mockArgs{
+				{
+					method:         "SetProject",
+					args:           []interface{}{context.Background(), mock.Anything},
+					paramsReturned: []interface{}{nil},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			mockModules := mockModulesInterface{}
+			mockStore := mockStoreInterface{}
+
+			for _, m := range tt.modulesMockArgs {
+				mockModules.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+			for _, m := range tt.storeMockArgs {
+				mockStore.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+			for _, m := range tt.schemaMockArgs {
+				mockSchema.On(m.method, m.args...).Return(m.paramsReturned...)
+			}
+
+			tt.s.modules = &mockModules
+			tt.s.store = &mockStore
+
+			if err := tt.s.SetModifyAllSchema(tt.args.ctx, tt.args.dbAlias, tt.args.project, tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("Manager.SetModifyAllSchema() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			mockModules.AssertExpectations(t)
+			mockStore.AssertExpectations(t)
+			mockSchema.AssertExpectations(t)
+		})
+	}
+}
