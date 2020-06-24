f000d4d3ac31db0f9277a97ae61cbd1fd91e32d0
prithvi <prithvi@prithvi.com>
2020-06-24 12:49:41 +0530

    added support of zsh

--- space-cli/cmd/cmd.go
+++ space-cli/cmd/cmd.go
@@ -26,41 +26,9 @@ func GetRootCommand() *cobra.Command {
 		},
 	}
 
-	// var completionCmd = &cobra.Command{
-	// 	Use:   "completion",
-	// 	Short: "Shell completion code",
-	// 	// RunE: func(cmd *cobra.Command, args []string) error {
-	// 	// 	return rootCmd.GenBashCompletionFile("/home/prithvi/project/space-cloud/out.sh")
-	// 	// },
-	// }
-	// var bashCmd = &cobra.Command{
-	// 	Use:   "bash",
-	// 	Short: "Bash shell completion code",
-	// 	RunE: func(cmd *cobra.Command, args []string) error {
-	// 		return rootCmd.GenBashCompletionFile("out.sh")
-	// 	},
-	// }
-	// var zshCmd = &cobra.Command{
-	// 	Use:   "zsh",
-	// 	Short: "Zsh shell completion code",
-	// 	RunE: func(cmd *cobra.Command, args []string) error {
-	// 		return rootCmd.GenZshCompletionFile("out1.sh")
-	// 	},
-	// }
-	// var powershellCmd = &cobra.Command{
-	// 	Use:   "powershell",
-	// 	Short: "Powershell shell completion code",
-	// 	RunE: func(cmd *cobra.Command, args []string) error {
-	// 		return rootCmd.GenPowerShellCompletionFile("out2.sh")
-	// 	},
-	// }
-
 	var completionCmd = &cobra.Command{
 		Use:   "completion [bash|zsh] [--no-descriptions]",
 		Short: "Generate completion script",
-		// DisableFlagsInUseLine: true,
-		// ValidArgs:             []string{"bash", "zsh1\tOriginal zsh completion", "zsh2\tV2 zsh completion", "fish"},
-		// Args:                  cobra.ExactValidArgs(1),
 		PreRun: func(cmd *cobra.Command, args []string) {
 			err := viper.BindPFlag("file", cmd.Flags().Lookup("file"))
 			if err != nil {
@@ -71,17 +39,19 @@ func GetRootCommand() *cobra.Command {
 			switch args[0] {
 			case "bash":
 				file := viper.GetString("file")
-				rootCmd.GenBashCompletionFile(fmt.Sprintf("%s/out.sh", file))
+				if file == "" {
+					_ = utils.LogError("file path not provided ()", nil)
+				}
+				rootCmd.GenBashCompletionFile(fmt.Sprintf("%s", file))
 				break
 			case "zsh":
 				file := viper.GetString("file")
-				fmt.Printf(fmt.Sprintf("%s/out.sh", file))
-				rootCmd.GenZshCompletionFile(fmt.Sprintf("%s/_space-cli", file))
+				rootCmd.GenZshCompletionFile(fmt.Sprintf("%s", file))
 				break
 			}
 		},
 	}
-	completionCmd.Flags().StringP("file", "", "/etc/bash_completion.d", "")
+	completionCmd.Flags().StringP("file", "", "", "")
 
 	rootCmd.PersistentFlags().StringP("log-level", "", "info", "Sets the log level of the command")
 	err := viper.BindPFlag("log-level", rootCmd.PersistentFlags().Lookup("log-level"))
