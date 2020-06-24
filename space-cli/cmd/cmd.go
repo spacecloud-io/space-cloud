8e0d7f833766d16fa5d83b618516d20bc29d8c03
prithvi <prithvi@prithvi.com>
2020-06-24 11:40:04 +0530

    minor changes

--- space-cli/cmd/cmd.go
+++ space-cli/cmd/cmd.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -24,34 +26,62 @@ func GetRootCommand() *cobra.Command {
 		},
 	}
 
+	// var completionCmd = &cobra.Command{
+	// 	Use:   "completion",
+	// 	Short: "Shell completion code",
+	// 	// RunE: func(cmd *cobra.Command, args []string) error {
+	// 	// 	return rootCmd.GenBashCompletionFile("/home/prithvi/project/space-cloud/out.sh")
+	// 	// },
+	// }
+	// var bashCmd = &cobra.Command{
+	// 	Use:   "bash",
+	// 	Short: "Bash shell completion code",
+	// 	RunE: func(cmd *cobra.Command, args []string) error {
+	// 		return rootCmd.GenBashCompletionFile("out.sh")
+	// 	},
+	// }
+	// var zshCmd = &cobra.Command{
+	// 	Use:   "zsh",
+	// 	Short: "Zsh shell completion code",
+	// 	RunE: func(cmd *cobra.Command, args []string) error {
+	// 		return rootCmd.GenZshCompletionFile("out1.sh")
+	// 	},
+	// }
+	// var powershellCmd = &cobra.Command{
+	// 	Use:   "powershell",
+	// 	Short: "Powershell shell completion code",
+	// 	RunE: func(cmd *cobra.Command, args []string) error {
+	// 		return rootCmd.GenPowerShellCompletionFile("out2.sh")
+	// 	},
+	// }
+
 	var completionCmd = &cobra.Command{
-		Use:   "completion",
-		Short: "Shell completion code",
-		// RunE: func(cmd *cobra.Command, args []string) error {
-		// 	return rootCmd.GenBashCompletionFile("/home/prithvi/project/space-cloud/out.sh")
-		// },
-	}
-	var bashCmd = &cobra.Command{
-		Use:   "bash",
-		Short: "Bash shell completion code",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return rootCmd.GenBashCompletionFile("out.sh")
+		Use:   "completion [bash|zsh] [--no-descriptions]",
+		Short: "Generate completion script",
+		// DisableFlagsInUseLine: true,
+		// ValidArgs:             []string{"bash", "zsh1\tOriginal zsh completion", "zsh2\tV2 zsh completion", "fish"},
+		// Args:                  cobra.ExactValidArgs(1),
+		PreRun: func(cmd *cobra.Command, args []string) {
+			err := viper.BindPFlag("file", cmd.Flags().Lookup("file"))
+			if err != nil {
+				_ = utils.LogError("Unable to bind the flag ('file')", err)
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				file := viper.GetString("file")
+				rootCmd.GenBashCompletionFile(fmt.Sprintf("%s/out.sh", file))
+				break
+			case "zsh":
+				file := viper.GetString("file")
+				fmt.Printf(fmt.Sprintf("%s/out.sh", file))
+				rootCmd.GenZshCompletionFile(fmt.Sprintf("%s/_space-cli", file))
+				break
+			}
 		},
 	}
-	var zshCmd = &cobra.Command{
-		Use:   "zsh",
-		Short: "Zsh shell completion code",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return rootCmd.GenZshCompletionFile("out1.sh")
-		},
-	}
-	var powershellCmd = &cobra.Command{
-		Use:   "powershell",
-		Short: "Powershell shell completion code",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return rootCmd.GenPowerShellCompletionFile("out2.sh")
-		},
-	}
+	completionCmd.Flags().StringP("file", "", "/etc/bash_completion.d", "")
 
 	rootCmd.PersistentFlags().StringP("log-level", "", "info", "Sets the log level of the command")
 	err := viper.BindPFlag("log-level", rootCmd.PersistentFlags().Lookup("log-level"))
@@ -80,9 +110,6 @@ func GetRootCommand() *cobra.Command {
 	rootCmd.AddCommand(operations.Commands()...)
 	rootCmd.AddCommand(login.Commands()...)
 	rootCmd.AddCommand(accounts.Commands()...)
-	completionCmd.AddCommand(bashCmd)
-	completionCmd.AddCommand(zshCmd)
-	completionCmd.AddCommand(powershellCmd)
 	rootCmd.AddCommand(completionCmd)
 	return rootCmd
 }
