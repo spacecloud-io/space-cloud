f7fe0b1e87e4ea30fb42f0d8f277f861a4abe4a7
prithvi <prithvi@prithvi.com>
2020-07-01 12:10:04 +0530

    conflict solved

--- gateway/utils/handlers/config_project.go
+++ gateway/utils/handlers/config_project.go
@@ -85,3 +85,62 @@ func HandleDeleteProjectConfig(adminMan *admin.Manager, syncMan *syncman.Manager
 		_ = utils.SendErrorResponse(w, http.StatusInternalServerError, "Operation not supported")
 	}
 }
+
+// HandleGetClusterConfig returns handler to get cluster-config
+func HandleGetClusterConfig(adminMan *admin.Manager, syncMan *syncman.Manager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Get the JWT token from header
+		token := utils.GetTokenFromHeader(r)
+
+		// Check if the request is authorised
+		if err := adminMan.IsTokenValid(token, "project", "modify", map[string]string{}); err != nil {
+			_ = utils.SendErrorResponse(w, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		clusterConfig, err := syncMan.GetClusterConfig()
+		if err != nil {
+			_ = utils.SendErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		_ = utils.SendResponse(w, http.StatusOK, model.Response{Result: clusterConfig})
+	}
+}
+
+// HandleSetClusterConfig set cluster-config
+func HandleSetClusterConfig(adminMan *admin.Manager, syncMan *syncman.Manager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Get the JWT token from header
+		token := utils.GetTokenFromHeader(r)
+
+		// Load the request from the body
+		req := new(config.ClusterConfig)
+		err := json.NewDecoder(r.Body).Decode(req)
+		defer utils.CloseTheCloser(r.Body)
+
+		// Throw error if request was of incorrect type
+		if err != nil {
+			_ = utils.SendErrorResponse(w, http.StatusBadRequest, "Admin Config was of invalid type - "+err.Error())
+			return
+		}
+
+		// Check if the request is authorised
+		if err := adminMan.IsTokenValid(token, "project", "modify", map[string]string{}); err != nil {
+			_ = utils.SendErrorResponse(w, http.StatusUnauthorized, err.Error())
+			return
+		}
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		// Sync the Adminconfig
+		if err := syncMan.SetClusterConfig(ctx, req); err != nil {
+			_ = utils.SendErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		_ = utils.SendResponse(w, http.StatusOK, map[string]interface{}{})
+	}
+}
