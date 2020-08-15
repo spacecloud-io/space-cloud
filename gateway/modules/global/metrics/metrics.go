747d53d83785d0691ffb9d16e1445b1448738a12
sharadregoti <sharadregoti15@gmail.com>
2020-08-15 08:53:50 +0530

    Linter fixes

--- gateway/modules/global/metrics/metrics.go
+++ gateway/modules/global/metrics/metrics.go
@@ -37,7 +37,7 @@ type Config struct {
 // New creates a new instance of the metrics module
 func New(clusterID, nodeID string, isMetricDisabled bool, adminMan *admin.Manager, syncMan *syncman.Manager, isProd bool) (*Module, error) {
 	// Initialise the sink
-	conn := api.New("spacecloud", "testing.spaceuptech.com", true).DB("db")
+	conn := api.New("spacecloud", "api.spaceuptech.com", true).DB("db")
 
 	// Create a new metrics module
 	m := &Module{nodeID: nodeID, clusterID: clusterID, sink: conn, isMetricDisabled: isMetricDisabled, adminMan: adminMan, syncMan: syncMan, isProd: isProd}
