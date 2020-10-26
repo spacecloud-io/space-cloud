b67e96760b8e7eec92f5f57fb050074a4b127069
KhushalPShah <khushalshah021098@gmail.com>
2020-10-26 10:40:33 +0530

    Commenting unused variable

--- gateway/modules/crud/sql/sql.go
+++ gateway/modules/crud/sql/sql.go
@@ -134,7 +134,7 @@ func (s *SQL) connect() error {
 
 	s.client.SetMaxOpenConns(maxConn)
 	s.client.SetMaxIdleConns(maxIdleConn)
-	duration := time.Duration(maxIdleTimeout) * time.Millisecond
+	// duration := time.Duration(maxIdleTimeout) * time.Millisecond
 	// s.client.SetConnMaxIdleTime(duration)
 	return sql.PingContext(ctx)
 }
