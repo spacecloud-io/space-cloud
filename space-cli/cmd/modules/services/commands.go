55d77196af75887fb6d7da9d2a0c4f9a2cba627b
Kavish Shah <kavishshah09@gmail.com>
2020-11-09 18:52:02 +0530

    Added deleters for Letsencrypt, project, remote-services, secrets deleters (#1385)

--- space-cli/cmd/modules/services/commands.go
+++ space-cli/cmd/modules/services/commands.go
@@ -99,24 +99,9 @@ func GetSubCommands() []*cobra.Command {
 	}
 
 	var getServicesSecrets = &cobra.Command{
-		Use:  "secrets",
-		RunE: actionGetServicesSecrets,
-		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-			project, check := utils.GetProjectID()
-			if !check {
-				utils.LogDebug("Project not specified in flag", nil)
-				return nil, cobra.ShellCompDirectiveDefault
-			}
-			obj, err := GetServicesSecrets(project, "secret", map[string]string{})
-			if err != nil {
-				return nil, cobra.ShellCompDirectiveDefault
-			}
-			var ids []string
-			for _, v := range obj {
-				ids = append(ids, v.Meta["id"])
-			}
-			return ids, cobra.ShellCompDirectiveDefault
-		},
+		Use:               "secrets",
+		RunE:              actionGetServicesSecrets,
+		ValidArgsFunction: secretsAutoCompleteFun,
 	}
 
 	var getServices = &cobra.Command{
@@ -311,4 +296,32 @@ func actionGenerateServiceRole(cmd *cobra.Command, args []string) error {
 	}
 
 	return utils.AppendConfigToDisk(serviceRole, serviceConfigFile)
+}
+
+// DeleteSubCommands is the list of commands the services module exposes
+func DeleteSubCommands() []*cobra.Command {
+	var deleteServicesSecrets = &cobra.Command{
+		Use:               "secrets",
+		Aliases:           []string{"secret"},
+		RunE:              actionDeleteServicesSecrets,
+		ValidArgsFunction: secretsAutoCompleteFun,
+		Example:           "space-cli delete secrets secretID --project myproject",
+	}
+
+	return []*cobra.Command{deleteServicesSecrets}
+}
+
+func actionDeleteServicesSecrets(cmd *cobra.Command, args []string) error {
+	// Get the project
+	project, check := utils.GetProjectID()
+	if !check {
+		return utils.LogError("Project not specified in flag", nil)
+	}
+
+	prefix := ""
+	if len(args) != 0 {
+		prefix = args[0]
+	}
+
+	return deleteSecret(project, prefix)
 }
