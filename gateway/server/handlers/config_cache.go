f96cd51daf9707d2b72ab27a66a7d8c7f2eac2f0
sharadregoti <sharadregoti15@gmail.com>
2020-11-10 13:21:12 +0530

    Changed the response structure of cache

--- gateway/server/handlers/config_cache.go
+++ gateway/server/handlers/config_cache.go
@@ -4,25 +4,27 @@ import (
 	"net/http"
 
 	"github.com/spaceuptech/helpers"
+
+	"github.com/spaceuptech/space-cloud/gateway/model"
 )
 
 // HandleSetCacheConfig is an endpoint handler which sets cache config
 func HandleSetCacheConfig() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_ = helpers.Response.SendErrorResponse(r.Context(), w, http.StatusBadRequest, "Not implemented in gateway")
+		_ = helpers.Response.SendErrorResponse(r.Context(), w, http.StatusBadRequest, "Caching module is disabled")
 	}
 }
 
 // HandleGetCacheConfig is an endpoint handler which gets cache config
 func HandleGetCacheConfig() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_ = helpers.Response.SendErrorResponse(r.Context(), w, http.StatusBadRequest, "Not implemented in gateway")
+		_ = helpers.Response.SendResponse(r.Context(), w, http.StatusOK, model.Response{Result: []interface{}{map[string]interface{}{"enabled": false}}})
 	}
 }
 
 // HandleGetCacheConnectionState is an endpoint handler returns connection status
 func HandleGetCacheConnectionState() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_ = helpers.Response.SendErrorResponse(r.Context(), w, http.StatusBadRequest, "Not implemented in gateway")
+		_ = helpers.Response.SendResponse(r.Context(), w, http.StatusOK, model.Response{Result: false})
 	}
 }
