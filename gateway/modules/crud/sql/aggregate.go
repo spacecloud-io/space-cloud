6c21efbf138d8400a4b9b9c62f1d1c9697b13e50
Noorain Panjwani <noorain.panjwani@gmail.com>
2021-07-21 11:00:53 +0530

    Update gateway/modules/crud/sql/aggregate.go

--- gateway/modules/crud/sql/aggregate.go
+++ gateway/modules/crud/sql/aggregate.go
@@ -9,5 +9,5 @@ import (
 
 // Aggregate performs a mongo db pipeline aggregation
 func (s *SQL) Aggregate(ctx context.Context, col string, req *model.AggregateRequest) (interface{}, error) {
-	return nil, errors.New("aggregation is not supported for nosql databases")
+	return nil, errors.New("aggregation is not supported for sql databases")
 }
