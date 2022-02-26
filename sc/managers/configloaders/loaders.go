310469d30cdb58d4633d42d2e818e603c4664138
khannaabhi <abhishekakhanna@gmail.com>
2022-02-26 16:30:11 +0530

    review changes

--- sc/managers/configloaders/loaders.go
+++ sc/managers/configloaders/loaders.go
@@ -5,5 +5,5 @@ import (
 	_ "github.com/spacecloud-io/space-cloud/managers/configloaders/file"
 
 	// importing kube store module
-	_ "github.com/spacecloud-io/space-cloud/managers/configloaders/kubernetes"
+	_ "github.com/spacecloud-io/space-cloud/managers/configloaders/kube"
 )
