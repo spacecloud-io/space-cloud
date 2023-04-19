a01fbdb1a4d5f063bdb2915e01cbabef973dce71
Shubham Nazare <shubham4443@gmail.com>
2023-04-19 11:58:09 +0530

    implementation change of subscribe/unsubscribe events
    Signed-off-by: Shubham Nazare <shubham4443@gmail.com>

--- sc-poc/modules/pubsub/websocket.go
+++ sc-poc/modules/pubsub/websocket.go
@@ -80,8 +80,9 @@ func (a *App) getSubscriberAPI(channelPath, channelName string) *apis.API {
 		Name: fmt.Sprintf("%s-subscriber", channelName),
 		Path: fmt.Sprintf("/v1/pubsub/default%s/subscriber", channelPath),
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx, cancel := context.WithCancel(r.Context())
-			defer cancel()
+			ctx, cancel := context.WithCancel(context.Background())
+			cancel()
+			isCtxActive := false
 
 			// Create the websocket connection
 			conn, _, _, err := ws.UpgradeHTTP(r, w)
@@ -90,14 +91,17 @@ func (a *App) getSubscriberAPI(channelPath, channelName string) *apis.API {
 				return
 			}
 
-			subscribeStatus := make(chan bool)
+			subscribeStatus := false
 			// Go routine to receive subscribe/unsubscribe events
 			go func() {
 				for {
 					data, _, err := wsutil.ReadClientData(conn)
 					if err != nil {
 						a.logger.Error("could not read client data or the connection is closed", zap.String("channel", channelName), zap.Error(err))
-						cancel()
+						if isCtxActive {
+							cancel()
+							isCtxActive = false
+						}
 						return
 					}
 
@@ -109,62 +113,75 @@ func (a *App) getSubscriberAPI(channelPath, channelName string) *apis.API {
 
 					a.logger.Debug("event received", zap.String("event", string(message.Event)))
 					if message.Event == SubscribeEvent {
-						a.logger.Debug("subscription started", zap.String("channel", channelName))
-						subscribeStatus <- true
-					}
-					if message.Event == UnsubscribeEvent {
-						a.logger.Debug("unsubscribed to the channel", zap.String("channel", channelName))
-						subscribeStatus <- false
-					}
-				}
-			}()
-
-			for startSubscribe := range subscribeStatus {
-				if startSubscribe {
-					// Subscribe to the watermill channel for receiving messages and send
-					// it over the websocket connection
-					msgChan, err := a.Subscribe(ctx, uuid.NewString(), channelName, SubscribeOptions{})
-					if err != nil {
-						a.logger.Error("could not subscribe to the channel", zap.String("channel", channelName), zap.Error(err))
-						return
-					}
-					for {
-						select {
-						case status := <-subscribeStatus:
-							if !status {
-								// TODO: Stop subscribing and break out of loop
-								a.logger.Debug("stop subscribing")
-							}
-						case msg := <-msgChan:
-							// Write the message to the websocket connection
-							var data interface{}
-							err := json.Unmarshal(msg.Payload, &data)
-							if err != nil {
-								a.logger.Error("error unmarshaling data", zap.String("channel", channelName), zap.Error(err))
-								continue
-							}
-
-							pubMsg := PublishMessage{
-								ID:       msg.UUID,
-								MetaData: msg.Metadata,
-								Payload:  data,
-							}
-
-							b, err := json.Marshal(pubMsg)
-							if err != nil {
-								a.logger.Error("error marshaling data", zap.String("channel", channelName), zap.Error(err))
-								continue
-							}
-
-							err = wsutil.WriteServerText(conn, b)
+						// Check if user is already subscribed
+						if subscribeStatus {
+							err = wsutil.WriteServerText(conn, []byte("You are already subscribed"))
 							if err != nil {
 								a.logger.Error("could not send message to the websocket", zap.Error(err))
 							}
-							msg.Ack()
+							continue
 						}
+
+						ctx, cancel = context.WithCancel(context.Background())
+						subscribeStatus = true
+						isCtxActive = true
+
+						go func() {
+							// Subscribe to the watermill channel for receiving messages and send
+							// it over the websocket connection
+							msgChan, err := a.Subscribe(ctx, uuid.NewString(), channelName, SubscribeOptions{})
+							if err != nil {
+								a.logger.Error("could not subscribe to the channel", zap.String("channel", channelName), zap.Error(err))
+								return
+							}
+							a.logger.Debug("subscribed to the channel", zap.String("channel", channelName))
+
+							for {
+								select {
+								case <-ctx.Done():
+									// Unsubscribe event received. Close the go routine.
+									return
+								case msg := <-msgChan:
+									// Write the message to the websocket connection
+									var data interface{}
+									err := json.Unmarshal(msg.Payload, &data)
+									if err != nil {
+										a.logger.Error("error unmarshaling data", zap.String("channel", channelName), zap.Error(err))
+										continue
+									}
+
+									pubMsg := PublishMessage{
+										ID:       msg.UUID,
+										MetaData: msg.Metadata,
+										Payload:  data,
+									}
+
+									b, err := json.Marshal(pubMsg)
+									if err != nil {
+										a.logger.Error("error marshaling data", zap.String("channel", channelName), zap.Error(err))
+										continue
+									}
+
+									err = wsutil.WriteServerText(conn, b)
+									if err != nil {
+										a.logger.Error("could not send message to the websocket", zap.Error(err))
+									}
+									msg.Ack()
+								}
+							}
+
+						}()
+					}
+					if message.Event == UnsubscribeEvent {
+						a.logger.Debug("unsubscribed to the channel", zap.String("channel", channelName))
+						if isCtxActive {
+							cancel()
+							isCtxActive = false
+						}
+						subscribeStatus = false
 					}
 				}
-			}
+			}()
 		}),
 	}
 }
