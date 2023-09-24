cc8cdda889715a6e280f5f45b4165851824ada99
Shubham Nazare <shubham4443@gmail.com>
2023-09-24 17:29:15 +0530

    add plugins for typescript
    Signed-off-by: Shubham Nazare <shubham4443@gmail.com>

--- sc-poc/cmd/spacectl/commands/client/generate/driver/typescript/common.go
+++ sc-poc/cmd/spacectl/commands/client/generate/driver/typescript/common.go
@@ -14,6 +14,17 @@ func getTypeName(name string, skipFirst bool) string {
 
 		arr[i] = strings.Title(item)
 	}
+	s1 := strings.Join(arr, "")
+
+	arr = strings.Split(s1, "_")
+	for i, item := range arr {
+		if i == 0 && skipFirst {
+			arr[i] = item
+			continue
+		}
+
+		arr[i] = strings.Title(item)
+	}
 
 	return strings.Join(arr, "")
 }
--- sc-poc/cmd/spacectl/commands/client/generate/driver/typescript/plugins.go
+++ sc-poc/cmd/spacectl/commands/client/generate/driver/typescript/plugins.go
@@ -1,7 +1,37 @@
 package typescript
 
-import "github.com/spacecloud-io/space-cloud/pkg/apis/core/v1alpha1"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spacecloud-io/space-cloud/pkg/apis/core/v1alpha1"
+)
 
 func (t *Typescript) GeneratePlugins(plugins []v1alpha1.HTTPPlugin) (string, string, error) {
-	return "", "", nil
+	fileName := "plugins.ts"
+	var b strings.Builder
+
+	s :=
+		`interface PluginDetails {
+    name: string,
+    driver: string
+}
+
+export class Plugins {
+`
+
+	for _, plugin := range plugins {
+		driverName := getTypeName(plugin.Driver, false) + getTypeName(plugin.Name, false)
+		s += fmt.Sprintf("    %s = (): PluginDetails => {\n", driverName)
+		s += fmt.Sprintf("        const %s: PluginDetails = {\n", driverName)
+		s += fmt.Sprintf("            name: %q,\n", plugin.Name)
+		s += fmt.Sprintf("            driver: %q,\n", plugin.Driver)
+		s += "        }\n"
+		s += fmt.Sprintf("        return %s\n", driverName)
+		s += "    }\n\n"
+	}
+
+	s += "}\n"
+	_, _ = b.WriteString(s)
+	return b.String(), fileName, nil
 }
